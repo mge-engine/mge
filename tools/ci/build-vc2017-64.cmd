dd619466dc8a5cc8e8053aadb438af30ba4c4799
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-06-03 22:56:23 +0200

    more ci

--- tools/ci/build-vc2017-64.cmd
+++ tools/ci/build-vc2017-64.cmd
@@ -1,3 +1,4 @@
+SET PROJECT_ROOT=%1
 SET INCLUDE=
 SET LIB=
 call "d:\sw\vc2017\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
@@ -5,3 +6,7 @@ set CC=cl
 set CXX=cl
 SET HOME=%SYSTEMDRIVE%%HOMEPATH%
 SET PATH=D:\sw\gs\bin;D:\sw\dynamicrio\bin64;%PATH%
+CD /D "%PROJECT_ROOT%"
+IF NOT EXISTS "%PROJECT_ROOT%"\build ( MKDIR build )
+CD build
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWitheDebInfo ..
