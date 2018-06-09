b00adbbc0bac1d692732f97cca7a05c9b2a3a456
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-06-09 10:09:58 +0200

    LateX path missing

--- tools/ci/build-vc2017-64.cmd
+++ tools/ci/build-vc2017-64.cmd
@@ -5,7 +5,7 @@ call "d:\sw\vc2017\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 set CC=cl
 set CXX=cl
 SET HOME=%SYSTEMDRIVE%%HOMEPATH%
-SET PATH=D:\sw\gs\bin;D:\sw\dynamicrio\bin64;%PATH%
+SET PATH=D:\sw\gs\bin;D:\sw\dynamicrio\bin64;d:\sw\miktex\miktex\bin\x64;%PATH%
 ECHO %PROJECT_ROOT%
 CD /D "%PROJECT_ROOT%"
 MKDIR build
