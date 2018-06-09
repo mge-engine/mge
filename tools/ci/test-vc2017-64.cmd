00e091f2e527887053b52658fdb8beefe60e3261
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-06-09 15:10:22 +0200

    Run tests as part of the pipeline

---
+++ tools/ci/test-vc2017-64.cmd
@@ -0,0 +1,14 @@
+SET PROJECT_ROOT=%1
+SET INCLUDE=
+SET LIB=
+call "d:\sw\vc2017\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+set CC=cl
+set CXX=cl
+SET HOME=%SYSTEMDRIVE%%HOMEPATH%
+SET PATH=D:\sw\gs\bin;D:\sw\dynamicrio\bin64;d:\sw\miktex\miktex\bin\x64;%PATH%
+ECHO %PROJECT_ROOT%
+CD /D "%PROJECT_ROOT%"
+MKDIR build
+CD build
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake --build . --target test
