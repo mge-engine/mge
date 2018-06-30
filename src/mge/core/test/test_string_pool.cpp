e64ba9c4fd72fcd263299c3d94c1fbb02345421a
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-06-30 08:36:07 +0200

    Tests

--- src/mge/core/test/test_string_pool.cpp
+++ src/mge/core/test/test_string_pool.cpp
@@ -4,6 +4,8 @@
 #include "test/googletest.hpp"
 #include "mge/core/string_pool.hpp"
 
+using mge::string_pool;
+
 TEST(string_pool, construct)
 {
     string_pool sp;
@@ -15,6 +17,18 @@ TEST(string_pool, intern_c_string)
 
     const char *s1 = sp.intern("FOO");
     const char *s2 = sp.intern("FOO");
+
+    EXPECT_STREQ(s1, "FOO");
+    EXPECT_STREQ(s1, s2);
+    EXPECT_EQ(s1, s2);
+}
+
+TEST(string_pool, intern_cxx_string)
+{
+    string_pool sp;
+    std::string foo("FOO");
+    const char *s1 = sp.intern(foo);
+    const char *s2 = sp.intern(foo);
 
     EXPECT_STREQ(s1, "FOO");
     EXPECT_STREQ(s1, s2);
