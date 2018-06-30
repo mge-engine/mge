ebd35905f97f165a7691aa8740505b47c9e41d18
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-06-30 08:33:16 +0200

    Tests for string pool.

--- src/mge/core/test/test_string_pool.cpp
+++ src/mge/core/test/test_string_pool.cpp
@@ -2,3 +2,21 @@
 // Copyright (c) 2018 by Alexander Schroeder
 // All rights reserved.
 #include "test/googletest.hpp"
+#include "mge/core/string_pool.hpp"
+
+TEST(string_pool, construct)
+{
+    string_pool sp;
+}
+
+TEST(string_pool, intern_c_string)
+{
+    string_pool sp;
+
+    const char *s1 = sp.intern("FOO");
+    const char *s2 = sp.intern("FOO");
+
+    EXPECT_STREQ(s1, "FOO");
+    EXPECT_STREQ(s1, s2);
+    EXPECT_EQ(s1, s2);
+}
