c76dcb57453c8d075d3934293dcc8d789d1c38c8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-08-05 13:07:34 +0200

    Fixup fix copyright script

--- tools/fixcopyright.py
+++ tools/fixcopyright.py
@@ -1,4 +1,20 @@
+# mge - Modern Game Engine
+# Copyright (c) 2018 by Alexander Schroeder
+# All rights reserved.
 import os
+import sys
+
+dry_run = False
+
+def write_file(filename, lines):
+    print "updating %s" % filename
+    if dry_run:
+        return
+    f = open(filename, "w")
+    for l in lines:
+        f.write(l)
+    f.close()
+
 
 def process_make_file(filename):
     f = open(filename)
@@ -16,47 +32,44 @@ def process_make_file(filename):
                 headercomment = False
         else:
             curated_lines.append(l)
-    f = open(filename, "w")
-    for l in curated_lines:
-        f.write(l)
-    f.close()
-
+    write_file(filename, curated_lines)
 
 def process_cpp_source_file(filename):
     f = open(filename)
     lines = f.readlines()
     f.close()
     curated_lines=[]
-    curated_lines.append("// mge - Modern Game Engine\n")
-    curated_lines.append("// Copyright (c) 2018 by Alexander Schroeder\n")
-    curated_lines.append("// All rights reserved.\n")
     headercomment = True
+    headercomment_found = False
     for l in lines:
         if headercomment:
-            if not l.startswith("//"):
+            if "#pragma once" in l:
+                curated_lines.append(l)
+            elif not l.startswith("//"):
+                curated_lines.append("// mge - Modern Game Engine\n")
+                curated_lines.append("// Copyright (c) 2018 by Alexander Schroeder\n")
+                curated_lines.append("// All rights reserved.\n")
                 curated_lines.append(l)
                 headercomment = False
+                headercomment_found = True
         else:
             curated_lines.append(l)
 
-
-    if len(lines) > 0:
-
-        if "/*" in lines[0]:
-            headercomment = True
-            for l in lines:
-                if headercomment:
-                    if '*/' in l:
-                        headercomment = False
-                else:
+    if not headercomment_found == 0:
+        if len(lines) > 0:
+            if "/*" in lines[0]:
+                headercomment = True
+                for l in lines:
+                    if headercomment:
+                        if '*/' in l:
+                            headercomment = False
+                    else:
+                        curated_lines.append(l)
+            else:
+                for l in lines:
                     curated_lines.append(l)
-        else:
-            for l in lines:
-                curated_lines.append(l)
-        f = open(filename, "w")
-        for l in curated_lines:
-            f.write(l)
-        f.close()
+
+    write_file(filename, curated_lines)
 
 def process_c_source_file(filename):
     f = open(filename)
@@ -65,8 +78,9 @@ def process_c_source_file(filename):
     curated_lines=[]
     curated_lines.append("/* mge - Modern Game Engine\n")
     curated_lines.append(" * Copyright (c) 2018 by Alexander Schroeder\n")
-    curated_lines.append(" * All rights reserved.\n
+    curated_lines.append(" * All rights reserved.\n")
     curated_lines.append(" */\n")
+
     if len(lines) > 0:
         if "/*" in lines[0]:
             headercomment = True
@@ -79,10 +93,8 @@ def process_c_source_file(filename):
         else:
             for l in lines:
                 curated_lines.append(l)
-        f = open(filename, "w")
-        for l in curated_lines:
-            f.write(l)
-        f.close()
+
+    write_file(filename, curated_lines)
 
 
 def process_file(filename):
@@ -103,6 +115,8 @@ def blacklisted(path):
     else:
         return False
 
+if "-n" in sys.argv:
+    dry_run = True
 
 for root, dirs, files in os.walk("."):
     path = root.split(os.sep)
@@ -113,6 +127,6 @@ for root, dirs, files in os.walk("."):
         filelist = filelist[1:]
         if(not blacklisted(filelist)):
             currentFile = "/".join(filelist)
-            print("%s" % currentFile)
+            print("processing %s" % currentFile)
             process_file(currentFile)
 
