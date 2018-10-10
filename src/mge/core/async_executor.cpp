0f1ed4ecdecacf25c532a02c2cdb51f010b32247
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-10 20:56:22 +0200

    Async executor.

---
+++ src/mge/core/async_executor.cpp
@@ -0,0 +1,22 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#include "mge/core/async_executor.hpp"
+
+namespace mge {
+
+    class synchronous_executor : public async_executor
+    {
+        void await(const std::function<void()>& f)
+        {
+            f();
+        }
+    };
+
+    async_executor&
+    async_executor::synchronous_executor()
+    {
+        static class synchronous_executor ex;
+        return ex;
+    }
+}
---
+++ src/mge/core/async_executor.hpp
@@ -0,0 +1,33 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include <functional>
+#include "mge/core/dllexport.hpp"
+
+namespace mge {
+    /**
+     * An @c async_executor executes a function asynchronously, usually in
+     * a different thread than the current one.
+     */
+    class MGE_CORE_EXPORT async_executor
+    {
+    public:
+        /**
+         * Destructor.
+         */
+        virtual ~async_executor() = default;
+
+        /**
+         * Executes a function asynchronously.
+         * @param f function to execute
+         */
+        virtual void await(const std::function<void()>& f) = 0;
+
+        /**
+         * Get a synchronous executor.
+         * @return excutor that executes the given function synchronously
+         */
+        static async_executor& synchronous_executor();
+    };
+}
