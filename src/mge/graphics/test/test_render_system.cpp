a4c9f29e3491d085bfa76cb077ef977531f29924
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-12 19:19:06 +0200

    Graphics test.

---
+++ src/mge/graphics/test/test_render_system.cpp
@@ -0,0 +1,13 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#include "test/googletest.hpp"
+#include "mge/graphics/render_system.hpp"
+
+#if 0
+TEST(render_system, default_name)
+{
+    auto name = mge::render_system::default_name();
+    EXPECT_TRUE(!name.empty());:
+}
+#endif
