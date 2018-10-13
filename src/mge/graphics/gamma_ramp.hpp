469154ea4f1ccff34b04f50b8baa57dd5d29b4e8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-13 16:18:47 +0200

    Fix copyright

--- src/mge/graphics/gamma_ramp.hpp
+++ src/mge/graphics/gamma_ramp.hpp
@@ -1,6 +1,6 @@
-/*
- * Copyright (c) 2017 by Alexander Schroeder
- */
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
 #pragma once
 
 #include "mge/graphics/dllexport.hpp"
