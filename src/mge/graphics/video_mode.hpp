b9cdc2a012467dc4ff7142a14feeda9209ccdaaf
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-13 10:55:36 +0200

    Video mode.

---
+++ src/mge/graphics/video_mode.hpp
@@ -0,0 +1,19 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/render/dllexport.hpp"
+
+namespace mge {
+
+    /**
+     * @brief Video mode as settable to a monitor.
+     */
+    class MGE_GRAPHICS_EXPORT video_mode
+    {
+    public:
+        unsigned int width;
+        unsigned int height;
+        unsigned int refresh_rate;
+    };
+}
