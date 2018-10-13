aa7eb8b162220d8165a75e42ca1610306489f3b9
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-13 18:00:56 +0200

    Ideas on type registration interface.

--- src/mge/reflection/bind_std.cpp
+++ src/mge/reflection/bind_std.cpp
@@ -1,10 +1,16 @@
 #include <cstdint>
+#include "mge/reflection/type.hpp"
+
+#include <string>
 
 namespace mge {
     namespace reflection {
         void bind_std()
         {
-
+            type::get<std::string>()
+                .constructor()
+                .destructor()
+                .method("size", &std::string::size);
         }
     }
 }
