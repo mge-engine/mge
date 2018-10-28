aee7c4e3bac26d41e41a0b60e75e3f5f62517ac8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-10-28 21:06:55 +0100

    WIP: module loading

--- src/mge/core/module.cpp
+++ src/mge/core/module.cpp
@@ -2,11 +2,42 @@
 // Copyright (c) 2018 by Alexander Schroeder
 // All rights reserved.
 #include "mge/core/module.hpp"
+#include "mge/core/singleton.hpp"
+#include "mge/core/path.hpp"
+
+#include <vector>
 
 namespace mge {
+
+    class module_registry
+    {
+    public:
+        module_registry()
+        {
+            m_load_paths.emplace_back(path("."));
+            m_load_paths.emplace_back(path({".", "modules"}));
+
+            m_module_prefixes.emplace_back("mge_module");
+            m_module_prefixes.emplace_back("libmge_module");
+        }
+
+        ~module_registry() = default;
+
+        void load_all()
+        {
+
+        }
+
+    private:
+        std::vector<path> m_load_paths;
+        std::vector<std::string> m_module_prefixes;
+    };
+
+    static singleton<module_registry> s_module_registry;
+
     void
     module::load_all()
     {
-        // TODO load
+        s_module_registry->load_all();
     }
 }
--- src/mge/core/module.hpp
+++ src/mge/core/module.hpp
@@ -4,12 +4,28 @@
 #pragma once
 #include "mge/core/dllexport.hpp"
 #include "mge/core/types.hpp"
+#include "mge/core/shared_library.hpp"
+
+#include <vector>
 
 namespace mge {
+
+    MGE_DECLARE_REF(module);
 
     class MGE_CORE_EXPORT module
     {
     public:
+        /**
+         * Creates and loads the module.
+         * @param name module name
+         */
+        module(const std::string& name);
+        ~module();
+
         static void load_all();
+        static std::vector<module_ref> all_modules();
+    private:
+        std::string        m_name;
+        shared_library_ref m_library;
     };
 }
