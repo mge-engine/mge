c2ad9f125d887c3df778ad5f9d021ed473ee49d7
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-03 16:16:36 +0100

    filesystem error

---
+++ src/mge/core/filesystem_error.cpp
@@ -0,0 +1,5 @@
+#include "mge/core/filesystem_error.hpp"
+#include "mge/core/stdexceptions.hpp"
+namespace mge {
+    MGE_DEFINE_EXCEPTION(filesystem_error);
+}
---
+++ src/mge/core/filesystem_error.hpp
@@ -0,0 +1,34 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/core/dllexport.hpp"
+#include "mge/core/types.hpp"
+#include "mge/core/exception.hpp"
+
+namespace mge {
+
+    /**
+     * @brief File system error exception
+     *
+     * Thrown if a file system access failed.
+     */
+    class MGE_CORE_EXPORT filesystem_error : public ::mge::exception
+    {
+    public:
+        filesystem_error();
+        filesystem_error(const filesystem_error& e);
+        filesystem_error(filesystem_error&& e);
+        ~filesystem_error();
+
+        filesystem_error& operator=(const filesystem_error& e);
+
+        template <typename Info>
+        filesystem_error& operator <<(const Info& info)
+        {
+            mge::exception::operator << (info);
+            return *this;
+        }
+
+    };
+}
