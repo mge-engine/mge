e0d3a0fee202ee8da2d6d6750b720aa32c6b3fbb
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-03 16:24:44 +0100

    file access

---
+++ src/mge/core/file_access.cpp
@@ -0,0 +1,37 @@
+#include "mge/core/file_access.hpp"
+#include "mge/core/path.hpp"
+
+namespace mge {
+    file_access::file_access(const std::string& path)
+                    : m_path(path)
+    {}
+
+    file_access::file_access()
+    {}
+
+    file_access::~file_access()
+    {}
+
+    const std::string&
+    file_access::file_path() const
+    {
+        return m_path;
+    }
+
+    std::string file_access::file_name() const
+    {
+        path p(m_path);
+        return p.file_name();
+    }
+
+    void file_access::set_path(const std::string& path)
+    {
+        m_path = path;
+    }
+
+    std::string file_access::file_extension() const
+    {
+        path p(m_path);
+        return p.extension();
+    }
+}
