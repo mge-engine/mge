cb34eb42309442dbf880b57ddf03cd9e2cc2b704
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-10 22:23:30 +0100

    Load modules in test.

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -3,13 +3,14 @@
 // All rights reserved.
 #include "test/googletest.hpp"
 #include "mge/core/log.hpp"
-
+#include "mge/core/module.hpp"
 MGE_DEFINE_LOG(TEST);
-
 
 int main(int argc, char **argv)
 {
     try {
+        MGE_DEBUG_LOG(TEST) << "Run test " << argv[0];
+        mge::module::load_all();
         ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
     } catch(const std::exception& ex) {
