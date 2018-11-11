fb80ab0c3daa2a6aa95d3105b2cc5e132d561e4c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-11 22:33:36 +0100

    Really load module.

--- src/mge/core/module.cpp
+++ src/mge/core/module.cpp
@@ -56,10 +56,16 @@ namespace mge {
         void load_module(const file& module_file)
         {
             MGE_DEBUG_LOG(MGE) << "Loading module: " << module_file;
+            try {
+                m_loaded_modules.emplace_back(shared_library::load(module_file.path().string()));
+            } catch(...) {
+                MGE_WARNING_LOG(MGE) << "Load module " << module_file << " failed";
+            }
             MGE_DEBUG_LOG(MGE) << "Module " << module_file << " loaded";
         }
         std::vector<file> m_load_paths;
         std::vector<std::string> m_module_prefixes;
+        std::vector<shared_library_ref> m_loaded_modules;
     };
 
     static singleton<module_registry> s_module_registry;
