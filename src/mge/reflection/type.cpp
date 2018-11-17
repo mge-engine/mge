a7a0cd1db37b5b01541243f5d97732fa558d111d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-17 19:20:14 +0100

    reflection::type

---
+++ src/mge/reflection/type.cpp
@@ -0,0 +1,11 @@
+#include "mge/reflection/type.hpp"
+
+namespace mge {
+    namespace reflection {
+
+        type::type(const std::string& name)
+            :m_name(name)
+        {}
+
+    }
+}
--- src/mge/reflection/type.hpp
+++ src/mge/reflection/type.hpp
@@ -4,14 +4,26 @@
 #pragma once
 #include "mge/reflection/dllexport.hpp"
 #include "mge/reflection/types.hpp"
+#include "mge/reflection/reflection_fwd.hpp"
+
+#include <string>
 
 namespace mge {
     namespace reflection {
+
+        /**
+         * A type.
+         */
         class MGE_REFLECTION_EXPORT type
         {
         public:
-            static template <typename T>
-            type<T> get();
+            template <typename T>
+            static type& get();
+
+            type(const std::string& name);
+            ~type() = default;
+        private:
+            std::string m_name;
         };
 }
 }
