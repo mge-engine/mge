0e5542d8fe6e13372c35dd57e4c1c701d982c4d1
Alexander Schroeder <schroeder.alexander@googlemail.com>
2018-11-30 20:22:28 +0100

    expose file class

--- src/mge/reflection/bind_core.cpp
+++ src/mge/reflection/bind_core.cpp
@@ -1,6 +1,7 @@
 #include "mge/reflection/module.hpp"
 #include "mge/reflection/type.hpp"
 #include "mge/core/configuration.hpp"
+#include "mge/core/file.hpp"
 #include "mge/core/log_severity.hpp"
 namespace mge {
     namespace reflection {
@@ -34,6 +35,17 @@ namespace mge {
                                  (const char *) const)
                 .method("empty", &mge::configuration::empty)
                 .method("store", &mge::configuration::store);
+
+            type<mge::file>()
+                .constructor<const char *>()
+                .constructor<const std::string&>()
+                .constructor<const std::string&, const std::string&>()
+                .destructor()
+                .method("path", &mge::file::path)
+                .method("exists", &mge::file::exists)
+                .method("is_directory", &mge::file::is_directory)
+                .method("is_file", &mge::file::is_file)
+                .method("is_system_file", &mge::file::is_system_file);
 
             type<mge::log_severity>()
                 .enum_value("NONE", log_severity::NONE)
