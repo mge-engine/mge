ebc6c321abd1a56839befc4799b215b7564e5e2b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-03-03 14:19:07 +0100

    Mouse action.

---
+++ src/mge/input/mouse_action.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "mge/input/dllexport.hpp"
+#include <iosfwd>
+
+namespace mge {
+
+    enum class MGE_INPUT_EXPORT mouse_action
+    {
+        PRESS = 0,
+        RELEASE = 1,
+#ifndef DOUBLE_CLICK
+        DOUBLE_CLICK = 2,
+#endif
+        MOUSE_ACTION_DOUBLE_CLICK = 2
+    };
+
+    MGE_INPUT_EXPORT std::ostream& operator <<(std::ostream& os, const mouse_action& a);
+}
