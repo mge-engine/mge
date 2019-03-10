ab94aceee82dad8e911ab2945f33deafb8c6763a
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-03-10 10:24:43 +0100

    Make blackscreen sample terminate.

--- src/samples/blackscreen/blackscreen.cpp
+++ src/samples/blackscreen/blackscreen.cpp
@@ -1,3 +1,27 @@
 #include "mge/application/application.hpp"
+#include "mge/graphics/render_system.hpp"
+#include "mge/core/log.hpp"
+
+using namespace mge;
+
+MGE_DEFINE_LOG(BLACKSCREEN);
+
+class blackscreen : public application
+{
+public:
+    blackscreen() = default;
+
+    void setup() override
+    {
+        MGE_DEBUG_LOG(BLACKSCREEN) << "Setup blackscreen";
+
+        set_quit();
+    }
+
+private:
+    render_system_ref m_render_system;
+};
+
+MGE_REGISTER_IMPLEMENTATION(blackscreen, mge::application, blackscreen);
 
 MGE_MAINFUNCTION
