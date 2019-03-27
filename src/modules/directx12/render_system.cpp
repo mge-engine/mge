3cdebf461177ad6632209865b6a0b44d7e5ce85a
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-03-27 20:26:45 +0100

    add render system

---
+++ src/modules/directx12/render_system.cpp
@@ -0,0 +1,78 @@
+//#include "window.hpp"
+#include "dx12.hpp"
+#include "system_config.hpp"
+//#include "window.hpp"
+#include "mge/graphics/render_system.hpp"
+#include "mge/core/log.hpp"
+
+#include "win32/monitor.hpp"
+#include "win32/com_unique_ptr.hpp"
+
+
+MGE_USE_LOG(DX12);
+
+
+namespace dx12 {
+
+    class render_system : public mge::render_system
+    {
+    public:
+        render_system()
+        {
+            enable_debug_layer();
+        }
+
+        ~render_system()
+        {}
+
+        void configure(const mge::configuration& config)
+        {
+            m_config.configure(config);
+        }
+
+        monitor_collection_t monitors() const override
+        {
+            return ::platform::monitor::all_monitors();
+        }
+
+        mge::monitor_ref primary_monitor() const override
+        {
+            return platform::monitor::primary_monitor();
+        }
+
+        mge::window_ref create_window(const mge::rectangle& rect,
+                                      const mge::window_options& options) override
+        {
+            mge::window_ref result;// = std::make_shared<window>(rect, options, m_config);
+            return result;
+        }
+
+    private:
+        void enable_debug_layer()
+        {
+#ifndef MGE_RELEASE
+            ID3D12Debug *debug_controller;
+            MGE_DEBUG_LOG(DX12) << "Try to enable debug layer" << std::endl;
+            HRESULT rc = D3D12GetDebugInterface(IID_PPV_ARGS(&debug_controller));
+            if (SUCCEEDED(rc)) {
+                m_debug_controller.reset(debug_controller);
+                m_debug_controller->EnableDebugLayer();
+                MGE_DEBUG_LOG(DX12) << "Debug layer enabled" << std::endl;
+            } else {
+                MGE_ERROR_LOG(DX12) << "Failed to enable debug layer: " << rc << std::endl;
+            }
+#endif
+        }
+
+        COM_UNIQUE_PTR(ID3D12Debug) m_debug_controller;
+        system_config m_config;
+    };
+
+    MGE_REGISTER_IMPLEMENTATION(render_system,
+                                mge::render_system,
+                                directx12 dx12);
+
+    
+}
+
+
---
+++ src/modules/directx12/system_config.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "mge/core/configuration.hpp"
+
+namespace dx12 {
+    class system_config
+    {
+    public:
+        system_config()
+            :m_debug(false),
+             m_software_device(false)
+        {}
+
+        void configure(const mge::configuration& config)
+        {
+            m_debug = config.value<bool>("debug", false);
+            m_software_device = config.value<bool>("software_device", false);
+        }
+
+        bool debug() const
+        {
+            return m_debug;
+        }
+
+        bool software_device() const
+        {
+            return m_software_device;
+        }
+    private:
+        bool m_debug;
+        bool m_software_device;
+    };
+}
