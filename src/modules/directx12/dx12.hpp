7723a7dd0a80967cfab169c5ea89a005c109a014
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-03-27 20:18:16 +0100

    dx12

---
+++ src/modules/directx12/dx12.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#endif
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <dxgi1_3.h>
+#include <dxgi1_4.h>
+#include <dxgi1_5.h>
+#include <dxgi1_6.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include "d3dx12.h"
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+#include "win32/com_unique_ptr.hpp"
