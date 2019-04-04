06890302e1cd9ce2d7f400ae10103fbca2d86f45
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-04-05 00:24:40 +0200

    Add vc2019 environment

---
+++ tools/settings/buildenv-vc2019-64.cmd
@@ -0,0 +1,8 @@
+SET INCLUDE=
+SET LIB=
+call "d:\sw\vc2019\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+set CC=cl
+set CXX=cl
+SET HOME=%SYSTEMDRIVE%%HOMEPATH%
+SET PATH=D:\sw\gs\bin;D:\sw\dynamicrio\bin64;%PATH%
+
