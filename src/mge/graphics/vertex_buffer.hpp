540b2bc2ad955ea5fc785dc4dc87cc5fd1cb379e
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-04-14 11:14:10 +0200

    development continued

---
+++ src/mge/graphics/vertex_buffer.hpp
@@ -0,0 +1,11 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/graphics/hardware_buffer.hpp"
+namespace mge {
+
+    class MGE_GRAPHICS_EXPORT vertex_buffer
+        : public hardware_buffer
+    {};
+}
