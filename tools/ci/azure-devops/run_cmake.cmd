964aaca5c898f1a8fe752dc3ff932d191c63576c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-08-24 22:54:33 +0200

    try run cmake

---
+++ tools/ci/azure-devops/run_cmake.cmd
@@ -0,0 +1,5 @@
+@SET VCPKG_CLONE_DIR="%1\vcpkg"
+@SET VCPKG_CMAKE_TOOLCHAIN_FILE="%1\vcpkg\scripts\buildsystems\vcpkg.cmake"
+rem BUILD_BINARIESDIRECTORY
+
+%VCPKG_CLONE_DIR%\vcpkg.exe env --triplet x64-windows
