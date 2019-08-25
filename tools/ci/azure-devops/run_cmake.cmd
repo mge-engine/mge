35bb6756e51fb500e4ad6fb0bbd2f27115f7fbbe
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-08-25 11:20:22 +0200

    clean start for ci

--- tools/ci/azure-devops/run_cmake.cmd
+++
@@ -1,7 +0,0 @@
-@SET VCPKG_CLONE_DIR="%1\vcpkg"
-@SET VCPKG_CMAKE_TOOLCHAIN_FILE="%1\vcpkg\scripts\buildsystems\vcpkg.cmake"
-rem BUILD_BINARIESDIRECTORY
-
-%VCPKG_CLONE_DIR%\vcpkg.exe env --triplet x64-windows
-
-cl /help
