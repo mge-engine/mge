888f2b0cee415676e9119cbabed44711a9597f0b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-09-24 19:31:05 +0200

    work on vulkan

--- src/modules/vulkan/render_system.cpp
+++ src/modules/vulkan/render_system.cpp
@@ -2,3 +2,54 @@
 // Copyright (c) 2018 by Alexander Schroeder
 // All rights reserved.
 #include "vulkan.hpp"
+#include "system_config.hpp"
+#include "mge/graphics/render_system.hpp"
+#include "mge/core/log.hpp"
+#ifdef MGE_OS_WINDOWS
+#  include "win32/monitor.hpp"
+#endif
+
+MGE_USE_LOG(VULKAN);
+
+namespace vulkan {
+
+    class render_system : public mge::render_system
+    {
+    public:
+        render_system()
+        {
+            MGE_DEBUG_LOG(VULKAN) << "Creating Vulkan render system";
+        }
+
+        virtual ~render_system() = default;
+
+        void configure(const mge::configuration& config) override
+        {
+            m_config.configure(config);
+        }
+
+        monitor_collection_t monitors() const override
+        {
+            return ::platform::monitor::all_monitors();
+        }
+
+        mge::monitor_ref primary_monitor() const override
+        {
+            return platform::monitor::primary_monitor();
+        }
+
+        mge::window_ref create_window(const mge::rectangle& rect,
+                                      const mge::window_options& options) override
+        {
+            mge::window_ref result;
+            return result;
+        }
+
+        system_config m_config;
+    };
+
+    MGE_REGISTER_IMPLEMENTATION(render_system,
+                                mge::render_system,
+                                vulkan);
+
+}
---
+++ src/modules/vulkan/system_config.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "mge/core/configuration.hpp"
+#include "mge/core/log.hpp"
+
+namespace vulkan {
+
+    class system_config
+    {
+    public:
+        system_config()
+            :m_debug(false)
+        {}
+        
+        void configure(const mge::configuration& config)
+        {
+           m_debug = config.value<bool>("debug", false);
+            if (config.contains_key("layers")) {
+                m_layers = config.list_value("layers");
+            }
+        }
+
+        bool debug() const
+        {
+            return m_debug;
+        }
+
+        const std::vector<std::string>& layers() const
+        {
+            return m_layers;
+        }
+    private:
+        bool m_debug;
+        std::vector<std::string> m_layers;
+    };
+
+}
