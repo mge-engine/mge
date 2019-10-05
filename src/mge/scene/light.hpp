77a43e04d173d8aaef11c7d9df8c11e3e918fe43
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-10-05 08:34:43 +0200

    further scene skeletons

---
+++ src/mge/scene/light.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "mge/scene/dllexport.hpp"
+namespace mge {
+
+    /**
+     * A light.
+     */
+    class MGE_SCENE_EXPORT light
+    {
+    };
+}
---
+++ src/mge/scene/material.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "mge/scene/dllexport.hpp"
+
+namespace mge {
+
+    /**
+     * A material. A material influences everything how the surface of a mesh
+     * look like, e.g. coloring and texture, and also surface parameters like
+     * shinyness.
+     */
+    class MGE_SCENE_EXPORT material
+    {
+
+    };
+}
