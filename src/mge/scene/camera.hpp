e53ef3196282705c6f7580d40cadd07e37991b64
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-10-05 15:34:33 +0200

    skeleton

--- src/mge/scene/camera.hpp
+++ src/mge/scene/camera.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include "mge/scene/dllexport.hpp"
+#include "mge/math/vec3.hpp"
+
 namespace mge {
     
     class MGE_SCENE_EXPORT camera
-    {};
+    {
+    public:
+        float m_aspect;
+        float m_near_plane;
+        float m_far_plane;
+        float m_fov;
+        fvec3 m_look_at;
+        fvec3 m_position;
+        fvec3 m_up;
+    };
 
 }
