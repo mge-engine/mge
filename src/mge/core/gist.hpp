5e31d8e89b9cc33ab5db7640936146098aacbaf8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-10-12 16:28:32 +0200

    development continued

--- src/mge/core/gist.hpp
+++ src/mge/core/gist.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "mge/core/types.hpp"
+#include "mge/core/type_name.hpp"
 #include <iostream>
+#include <type_traits>
+
 namespace mge {
 
     template <typename T>
@@ -19,10 +22,26 @@ namespace mge {
         return gist_type<T>(&t);
     }
 
+    namespace {
+        template <typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
+        void print_value(std::ostream& os, const T& value)
+        {
+            os << value;
+        }
+
+        template <typename T, std::enable_if_t<std::is_class_v<T>, bool> = true>
+        void print_value(std::ostream& os, const T& value)
+        {
+            os << mge::type_name<T>() << "@" << (void *)&value;
+        }
+    }
+
+
     template <typename T>
     std::ostream& operator <<(std::ostream& os, const gist_type<T>& g)
     {
-        return os << *g.value;
+        print_value(os, *g.value);
+        return os;
     }
 
 #define GIST_OUTPUT(TYPE)                                                   \
--- src/mge/core/test/test_gist.cpp
+++ src/mge/core/test/test_gist.cpp
@@ -10,9 +10,21 @@ using namespace mge;
 using namespace testing;
 using namespace std::string_literals;
 
+class foobar {};
+
+
 TEST(gist, gist_pod_type)
 {
     std::stringstream ss;
     ss << gist(123);
     EXPECT_EQ("123"s, ss.str());
+}
+
+TEST(gist, gist_class_type)
+{
+    foobar foo;
+    std::stringstream ss;
+    ss << gist(foo);
+    EXPECT_TRUE(ss.str().starts_with("foobar@"));
+
 }
