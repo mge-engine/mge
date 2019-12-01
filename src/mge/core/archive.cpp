80547b8fb0581a732ab389305ef484876abba345
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-12-01 21:48:19 +0100

    archive access factory continued

--- src/mge/core/archive.cpp
+++ src/mge/core/archive.cpp
@@ -1,5 +1,7 @@
 #include "mge/core/archive.hpp"
 #include "mge/core/archive_access_factory.hpp"
+#include "mge/core/singleton.hpp"
+
 #include <map>
 
 namespace mge {
@@ -12,11 +14,38 @@ namespace mge {
 
         ~factory_registry()
         {}
+
+        archive_access_factory_ref get_factory(const file& f)
+        {
+            auto fac = find_factory(f.path());
+            if (!fac) {
+                update_factories();
+                fac = find_factory(f.path());
+            }
+            return fac;
+        }
     private:
+        void update_factories()
+        {
+
+        }
+
+        archive_access_factory_ref find_factory(const path& p)
+        {
+            std::lock_guard<std::mutex> guard(m_lock);
+            for (const auto& e : m_access_factories) {
+                if (e.second->handles_path(p)) {
+                    return e.second;
+                }
+            }
+            return archive_access_factory_ref();
+        }
+
+        std::mutex                                        m_lock;
         std::map<std::string, archive_access_factory_ref> m_access_factories;
     };
 
-
+    static singleton<factory_registry> archive_factories;
 
     archive::entry::entry(const mge::path& p, bool dir)
         : m_path(p)
