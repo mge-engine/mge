368ea900882b45c922338f085f45b9c45a7497ad
Alexander Schroeder <schroeder.alexander@googlemail.com>
2019-12-01 11:35:04 +0100

    Zip archive handling continued.

--- src/modules/zip/zip.cpp
+++ src/modules/zip/zip.cpp
@@ -1,17 +1,39 @@
 #include "mge/core/archive_access_factory.hpp"
-
-namespace zip {
+#include <zip.h>
+// cannot use zip as namespace as that is already defined by zip.h
+namespace zip_archive {
 
     class zip_archive_access : 
         public mge::archive_access
     {
     public:
         zip_archive_access(const mge::file &f, mge::open_mode m)
-            :mge::archive_access(f, m)
-        {}
+            : mge::archive_access(f, m)
+            , m_zip_source(nullptr)
+            , m_source_delete_needed(true)
+        {
+            zip_error_t err = {};
+            m_zip_source = zip_source_function_create(&zip_source_callback, this, &err);
+            if (m_zip_source == nullptr) {
+
+            }
+        }
 
         virtual ~zip_archive_access()
-        {}
+        {
+            if (m_zip_source && m_source_delete_needed) {
+                zip_source_free(m_zip_source);
+            }
+        }
+
+    private:
+        static zip_int64_t zip_source_callback(void * arg1, void * arg2, zip_uint64_t arg3, zip_source_cmd_t cmd)
+        {
+            return -1;
+        }
+
+        zip_source_t* m_zip_source;
+        bool          m_source_delete_needed;
     };
 
     class archive_access_factory 
