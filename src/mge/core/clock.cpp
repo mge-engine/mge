c95121b7c58e7e439b49babe0b8207f68c736979
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-01-04 13:03:08 +0100

    Benchmark / improvements on clock.

--- src/mge/core/clock.cpp
+++ src/mge/core/clock.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2018 by Alexander Schroeder
 // All rights reserved.
 #include "mge/core/clock.hpp"
+#include "mge/core/unlikely.hpp"
 #include "mge/config.hpp"
 #include <chrono>
 #ifdef MGE_OS_WINDOWS
@@ -10,13 +11,20 @@
 
 namespace mge {
 
-    static auto epoch = std::chrono::high_resolution_clock::now();
+    static uint64_t s_epoch = 0;
 
     uint64_t clock::now()
     {
-        auto since_epoch = std::chrono::high_resolution_clock::now() - epoch;
-        return static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(since_epoch).count());
+        uint64_t current_epoch = s_epoch;
+        if (current_epoch) {
+            uint64_t current_now = static_cast<uint64_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+            return current_now - current_epoch;
+        }
+
+        s_epoch = static_cast<uint64_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+        return 0;
     }
+
 
     uint64_t clock::tick()
     {
