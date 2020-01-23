b390af08d225944fc6aff85948dba1fd90d7b7f5
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-01-23 15:39:11 +0100

    Fixup libzip

--- cmake/modules/libzip.cmake
+++ cmake/modules/libzip.cmake
@@ -1,9 +1,10 @@
 # mge - Modern Game Engine
 # Copyright (c) 2018 by Alexander Schroeder
 # All rights reserved.
-INCLUDE(FindPkgConfig)
-PKG_SEARCH_MODULE(LIBZIP REQUIRED libzip)
 
+# libzip has no good package finding mechanism
+FIND_PATH(LIBZIP_INCLUDE_DIRS zip.h)
+FIND_LIBRARY(LIBZIP_LINK_LIBRARIES zip)
 ADD_LIBRARY(libzip UNKNOWN IMPORTED)
 SET_PROPERTY(TARGET libzip
              PROPERTY IMPORTED_LOCATION "${LIBZIP_LINK_LIBRARIES}")
