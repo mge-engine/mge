5bded28a10a581326b1bb7794f6504e3858250f6
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-01-25 09:02:45 +0100

    Protect constructor.

--- src/mge/core/module.hpp
+++ src/mge/core/module.hpp
@@ -14,16 +14,43 @@ namespace mge {
 
     class MGE_CORE_EXPORT module
     {
-    public:
+
         /**
          * Creates and loads the module.
          * @param name module name
          */
+    protected:
         module(const std::string& name);
+    public:
         ~module();
 
+        /**
+         * @brief Load all modules.
+         *
+         * Scans configuration and loads all modules.
+         */
         static void load_all();
+
+        /**
+         * @brief Retrieve all loaded modules.
+         *
+         * @return vector of loaded modules
+         */
         static std::vector<module_ref> all_modules();
+
+        /**
+         * @brief Module name.
+         *
+         * @return module name
+         */
+        const std::string& name() const;
+
+        /**
+         * @brief Module shared library.
+         *
+         * @return Shared library implemnting the module.
+         */
+        const mge::shared_library_ref library() const;
     private:
         std::string        m_name;
         shared_library_ref m_library;
