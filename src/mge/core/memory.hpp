51dc6b10120085a071b7e75ab79d72f34c222eb7
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-01-29 22:34:38 +0100

    Documentation.

--- src/mge/core/memory.hpp
+++ src/mge/core/memory.hpp
@@ -12,9 +12,36 @@
 
 namespace mge {
 
+    /**
+     * @brief Allocate memory.
+     *
+     * @param sz number of bytes to allocate
+     * @return pointer allocated memory, NULL if not possible to allocate
+     */
     MGE_CORE_EXPORT void *malloc(size_t sz);
+    /**
+     * @brief Free memory.
+     *
+     * @param ptr pointer to free
+     */
     MGE_CORE_EXPORT void free(void *ptr);
+    /**
+     * @brief Reallocate memory buffer.
+     *
+     * @param ptr   pointer to previously allocated memory
+     * @param newsz new size
+     * @return pointer to reallocated memory, may be same or different from
+     *   passed pointer
+     */
     MGE_CORE_EXPORT void *realloc(void *ptr, size_t newsz);
+
+    /**
+     * @brief Allocate memory and initialize.
+     *
+     * @param num   number of elements
+     * @param size  size of one element
+     * @return pointer to allocated and cleared memory
+     */
     MGE_CORE_EXPORT void *calloc(size_t num, size_t size);
 
 }
