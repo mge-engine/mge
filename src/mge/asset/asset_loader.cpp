d732d37a2e851738a2a64eec3bd794fd942c4da2
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-03-01 10:00:18 +0100

    Loader resolution.

--- src/mge/asset/asset_loader.cpp
+++ src/mge/asset/asset_loader.cpp
@@ -2,6 +2,8 @@
 #include "mge/asset/asset.hpp"
 #include "mge/core/singleton.hpp"
 #include <map>
+#include <set>
+
 namespace mge {
     MGE_REGISTER_COMPONENT(asset_loader);
 
@@ -16,10 +18,46 @@ namespace mge {
             auto it = m_loaders.find(t);
             if (it != m_loaders.end()) {
                 return it->second;
+            } else {
+                auto updated = update_loaders();
+                if (updated) {
+                    return find_loader(t);
+                }
             }
             return asset_loader_ref();
         }
     private:
+        bool update_loaders()
+        {
+            std::vector<std::string> new_implementations;
+            asset_loader::implementations([&](const std::string& name){
+                if(m_loader_implementations.find(name) == m_loader_implementations.end()) {
+                    new_implementations.emplace_back(name);
+                }
+            });
+            if (new_implementations.empty()) {
+                return false;
+            }
+            for(const auto& name : new_implementations) {
+                add_loader(name);
+            }
+            return true;
+        }
+
+        void add_loader(const std::string& name)
+        {
+            auto loader = asset_loader::create(name);
+            std::vector<asset_type> handled_types;
+            loader->handled_types([&](const asset_type& t){
+                handled_types.emplace_back(t);
+            });
+            m_loader_implementations.insert(name);
+            for(const auto& t : handled_types) {
+                m_loaders[t] = loader;
+            }
+        }
+
+        std::set<std::string> m_loader_implementations;
         std::map<asset_type, asset_loader_ref> m_loaders;
     };
 
