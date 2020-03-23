457f1edf29f63a190de670e4d77d876dadd29c55
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-03-23 20:36:13 +0100

    Freetype library setup finished

--- cmake/modules/freetype.cmake
+++ cmake/modules/freetype.cmake
@@ -8,3 +8,18 @@ SET_PROPERTY(TARGET freetype
              PROPERTY IMPORTED_LOCATION "${FREETYPE_LINK_LIBRARIES}")
 SET_PROPERTY(TARGET freetype
              APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${FREETYPE_INCLUDE_DIRS}")
+#INCLUDE(functions)
+#IF(WIN32)
+#    IF(VCPKG_TOOLCHAIN STREQUAL "ON")
+##_VCPKG_INSTALLED_DIR
+#        MESSAGE("-- Setup freetype libraries from vcpkg")
+#        COPY_FILE("${_VCPKG_INSTALLED_DIR}/x64-windows/bin/freetype.dll"
+#            "${CMAKE_BINARY_DIR}/freetype.dll")
+#        COPY_FILE("${_VCPKG_INSTALLED_DIR}/x64-windows/bin/freetype.pdb"
+#            "${CMAKE_BINARY_DIR}/freetype.pdb")
+#    ELSE()
+#        MESSAGE(FATAL_ERROR "Need vcpkg info for freetype")
+#    ENDIF()
+#ELSE()
+#    MESSAGE(FATAL_ERROR "Unsupported Setup")
+#ENDIF()
