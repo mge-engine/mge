0f34088eb432abf77d69ed7d3cb672e732bcb20d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-03-28 17:08:56 +0100

    Development continued.

--- src/mge/core/license.cpp
+++ src/mge/core/license.cpp
@@ -1,7 +1,51 @@
 #include "mge/core/license.hpp"
+#include <iostream>
 
 namespace mge {
+
+    std::ostream& operator <<(std::ostream& os, license_type type)
+    {
+        switch(type) {
+        case license_type::UNKNOWN:         return os << "UNKNOWN";
+        case license_type::MIT:             return os << "MIT";
+        case license_type::BSD:             return os << "BSD";
+        case license_type::GPL2:            return os << "GPL2";
+        case license_type::LGPL2:           return os << "LGPL2";
+        case license_type::GPL3:            return os << "GPL3";
+        case license_type::LGPL3:           return os << "LGPL3";
+        case license_type::PUBLIC_DOMAIN:   return os << "PUBLIC_DOMAIN";
+        case license_type::CC:              return os << "CC";
+        case license_type::COMMERCIAL:      return os << "COMMERCIAL";
+        case license_type::OTHER:           return os << "OTHER";
+        default:
+                                            return os << "?";
+        }
+    }
+
     license::license()
-        :m_type(license::UNKNOWN)
+        :m_type(license_type::UNKNOWN)
     {}
+
+    license::license(license_type type)
+        :m_type(type)
+    {}
+
+    license_type
+    license::type() const
+    {
+        return m_type;
+    }
+
+    std::string_view
+    license::text() const
+    {
+        return m_text;
+    }
+
+    const license_properties&
+    license::properties() const
+    {
+        return m_properties;
+    }
+
 }
--- src/mge/core/license.hpp
+++ src/mge/core/license.hpp
@@ -1,8 +1,58 @@
 #pragma once
 #include "mge/core/dllexport.hpp"
+#include "mge/core/enum_set.hpp"
+#include "mge/core/version.hpp"
 #include <string_view>
+#include <iosfwd>
 
 namespace mge {
+
+    enum class license_type : uint8_t {
+        UNKNOWN,            //!< Yet unknown license.
+        MIT,                //!< MIT License
+        BSD,                //!< BSD License
+        GPL2,               //!< GPL 2 License
+        LGPL2,              //!< LGPL 2 License
+        GPL3,               //!< GPL 3 License
+        LGPL3,              //!< LGPL 3 License
+        PUBLIC_DOMAIN,      //!< Public Domain software
+        CC,                 //!< Any creative commons license
+        COMMERCIAL,         //!< Any commercial license
+        OTHER               //!< Other license.
+    };
+
+    enum class license_permission_value : uint8_t
+    {
+        USAGE            = 0,
+        REPRODUCTION     = 1,
+        DISTRIBUTION     = 2,
+        DERIVATIVE_WORKS = 3,
+    };
+
+    using license_permission = mge::enum_set<license_permission_value, 3>;
+
+    enum class license_requirement_value : uint8_t
+    {
+        NONE            = 0,
+        NOTICE          = 1,
+        ATTRIBUTION     = 2,
+        SHARE_ALIKE     = 3,
+        SOURCE_CODE     = 4
+    };
+
+    using license_requirement = mge::enum_set<license_requirement_value, 4>;
+
+    /**
+     * Properties of a license.
+     */
+    struct license_properties
+    {
+        license_permission  permissions;
+        license_requirement requirements;
+        bool                noncommercial;
+    };
+
+    MGE_CORE_EXPORT std::ostream& operator <<(std::ostream& os, license_type type);
 
     /**
      * License. An asset, component, library or application - any kind
@@ -11,35 +61,23 @@ namespace mge {
     class MGE_CORE_EXPORT license
     {
     public:
-        /**
-         * Well known license types.
-         */
-        enum license_type {
-            UNKNOWN,            //!< Yet unknown license.
-            MIT,                //!< MIT License
-            BSD,                //!< BSD License
-            GPL2,               //!< GPL 2 License
-            LGPL2,              //!< LGPL 2 License
-            GPL3,               //!< GPL 3 License
-            LGPL3,              //!< LGPL 3 License
-            PUBLIC_DOMAIN,      //!< Public Domain software
-            CC,                 //!< Any creative commons license
-            COMMERCIAL,         //!< Any commercial license
-            OTHER               //!< Other license.
-        };
-
         license();
         license(license_type type);
         license(license_type type,
-                std::string_view text);
+                std::string_view text,
+                const mge::version& version,
+                const license_properties& properties);
         ~license() = default;
         license(const license&) = default;
         license(license&&) = default;
 
         std::string_view text() const;
-
+        license_type type() const;
+        mge::version version() const;
+        const license_properties& properties() const;
     private:
-        license_type m_type;
-        std::string  m_text;
+        license_type       m_type;
+        std::string        m_text;
+        license_properties m_properties;
     };
 }
