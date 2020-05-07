08ae1c8e001027e5e9652fdc2505717054ac9894
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-05-08 00:40:28 +0200

    Fixup spelling (dynamo rio vs. dynamic)

--- cmake/modules/coverage.cmake
+++ cmake/modules/coverage.cmake
@@ -2,13 +2,13 @@
 # Copyright (c) 2018 by Alexander Schroeder
 # All rights reserved.
 OPTION(COVERAGE_TESTS "Run coverage tests to determine code coverage" ON)
-OPTION(DYNAMICRIO_PATH "Path to Dynamic Rio Tools" "")
+OPTION(DYNAMORIO_PATH "Path to Dynamo Rio Tools" "")
 IF(COVERAGE_TESTS)
     MESSAGE("-- Coverage tests are enabled")
-    IF(DYNAMICRIO_PATH)
+    IF(DYNAMIOIO_PATH)
         FIND_PROGRAM(DRRUN
             NAMES drrun.exe
-            PATH "${DYNAMICRIO_PATH}/bin64"
+            PATH "${DYNAMORIO_PATH}/bin64"
         )
     ELSE()
         FIND_PROGRAM(DRRUN
@@ -17,7 +17,7 @@ IF(COVERAGE_TESTS)
     ENDIF()
     FILE(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/coverage")
     IF(DRRUN STREQUAL "DRRUN-NOTFOUND")
-        MESSAGE("-- Coverage tests are disabled, dynamic rio tools not found")
+        MESSAGE("-- Coverage tests are disabled, dynamo rio tools not found")
     ELSE()
         GET_FILENAME_COMPONENT(DRRUN_PATH ${DRRUN} DIRECTORY)
         SET(DRCOV2LCOV "${DRRUN_PATH}/../tools/bin64/drcov2lcov.exe")
