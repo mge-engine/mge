c2e53edd4f8bf39ac6ae3af88c5d69a98d747562
Alexander Schroeder <schroeder.alexander@googlemail.com>
2020-05-29 19:00:34 +0200

    Contains implementation for additional data structures

--- src/mge/core/contains.hpp
+++ src/mge/core/contains.hpp
@@ -1,11 +1,22 @@
 // mge - Modern Game Engine
 // Copyright (c) 2018 by Alexander Schroeder
 // All rights reserved.
+#include "mge/core/small_vector.hpp"
 #include <algorithm>
+#include <array>
 #include <vector>
 
 namespace mge {
 
+    /**
+     * @brief Check whether an element is contained in a container.
+     *
+     * @tparam Container container type
+     * @tparam Element container element type
+     * @param c container
+     * @param e element
+     * @return @c true if the container contains the element
+     */
     template <typename Container, typename Element>
     inline bool contains(const Container &c, const Element &e)
     {
@@ -14,6 +25,31 @@ namespace mge {
 
     template <typename Element>
     inline bool contains(const std::vector<Element> &c, const Element &e)
+    {
+        auto it = c.begin();
+        while (it != c.end()) {
+            if (*it == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template <typename Element, size_t S, class Alloc>
+    inline bool contains(const mge::small_vector<Element, S, Alloc> &c,
+                         const Element &                             e)
+    {
+        auto it = c.begin();
+        while (it != c.end()) {
+            if (*it == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template <typename Element, size_t S>
+    inline bool contains(const std::array<Element, S> &c, const Element &e)
     {
         auto it = c.begin();
         while (it != c.end()) {
