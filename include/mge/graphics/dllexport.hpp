86afd9f91e34416134348d2a35b2238a8686c354
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-01-24 14:30:43 +0100

    Add graphics layer

---
+++ include/mge/graphics/dllexport.hpp
@@ -0,0 +1,17 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+
+#include "mge/dllexport.hpp"
+
+/**
+ * @def MGEGRAPHICS_EXPORT
+ * @brief Macro to declare class or function to be exported by/imported from
+ * core library.
+ */
+#ifdef BUILD_MGEGRAPHICS
+#    define MGEGRAPHICS_EXPORT MGE_DLLEXPORT
+#else
+#    define MGEGRAPHICS_EXPORT MGE_DLLIMPORT
+#endif
---
+++ include/mge/graphics/render_system.hpp
@@ -0,0 +1,5 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/graphics/dllexport.hpp"
