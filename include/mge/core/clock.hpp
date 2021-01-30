c963b6a5f529d312355201d73bceccb152d158ff
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-01-30 20:12:43 +0100

    Clock type definitio

---
+++ include/mge/core/clock.hpp
@@ -0,0 +1,11 @@
+// mge - Modern Game Engine
+// Copyright (c) 2019 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/core/dllexport.hpp"
+#include <chrono>
+namespace mge {
+
+    using clock = std::chrono::high_resolution_clock;
+
+}
