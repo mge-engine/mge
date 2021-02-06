b1589602d1719854e2103b30f80297fc85d7cb82
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-06 19:44:46 +0100

    complete implementation

--- tools/ci/vcpkg.py
+++ tools/ci/vcpkg.py
@@ -2,8 +2,13 @@
 import os
 import sys
 import subprocess
+import shutil
 
-version = 1
+# parameters
+# -- version            increment each time config changes
+# -- vcpkg_githash      git hash of vcpkg to check out
+# -- vcpkg_packages     array of package names to install
+version = "1"
 vcpkg_githash = "70422b96724101d67e88923539cf2ea2c880c39a"
 vcpkg_packages = [
     "gtest", "boost-preprocessor", "boost-filesystem",
@@ -21,17 +26,47 @@ def install_vcpkg():
     print("Bootstrapping vcpkg", flush=True)
     subprocess.run([".\\bootstrap-vcpkg.bat", "-disableMetrics"],
                    cwd="vcpkg", shell=True)
+    install_command = ["vcpkg.exe", "install", "--triplet", "x64-windows"]
+    install_command.extend(vcpkg_packages)
+    print("Installing following packages:", flush=True)
+    for p in vcpkg_packages:
+        print("    %s" % p, flush=True)
+    subprocess.run(install_command, cwd="vcpkg")
+    version_file = open("vcpkg/build-version", "w")
+    version_file.write(version)
+    version_file.close()
 
 
-if os.path.exists("vcpkg") and os.path.isdir("vcpkg"):
-    print("vcpkg path exists", flush=True)
-else:
-    print("vcpkg path not found, performing complete install", flush=True)
-    install_vcpkg()
+def is_vcpkg_installed():
+    try:
+        with open("vcpkg/build-version") as f:
+            installed_version = f.read().replace("\n", "")
+            print("found installed vcpkg version %s" %
+                  (installed_version), flush=True)
+            if installed_version == version:
+                print("required version matches installed version", flush=True)
+                return True
+            else:
+                print("required version is %s, need to install vcpkg" %
+                      version, flush=True)
+                return False
+    except:
+        return False
+
+
+try:
+    if not is_vcpkg_installed():
+        if os.path.exists("vcpkg") and os.path.isdir("vcpkg"):
+            print("vcpkg path exists, renoving it", flush=True)
+            shutil.rmtree("vcpkg")
+        print("performing complete install", flush=True)
+        install_vcpkg()
+except:
+    print(sys.exc_info, flush=True)
     sys.exit(1)
 
+sys.exit(0)
 
-sys.exit(1)
 
 #   - cmd: git clone https://github.com/mge-engine/vcpkg.git
 #  - cmd: cd vcpkg
