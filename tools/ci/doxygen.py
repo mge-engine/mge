d8c874c727c8012a03a82c8af0b44b11d826c373
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-06 23:36:15 +0100

    Install doxygen in python

---
+++ tools/ci/doxygen.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import subprocess
+import shutil
+
+doxygen_version = "1.9.1"
+
+
+def is_doxygen_installed():
+    try:
+        installed_version = subprocess.check_output(
+            [".\doxygen.exe", "--version"], cwd="C:\\PROGRA~1\\doxygen\\bin")
+        print("Doxygen %s found" % installed_version, flush=True)
+        if installed_version.startswith(doxygen_version):
+            return True
+        else:
+            return False
+    except:
+        print("Cannot determine doxygen version %s" % sys.exc_info, flush=True)
+        return False
+
+
+def install_doxygen():
+    print("Installing doxygen", flush=True)
+    subprocess.run(["choco", "install", "-y", "doxygen.install"])
+
+
+try:
+    if not is_doxygen_installed():
+        install_doxygen()
+    sys.exit(0)
+except:
+    print("Exception %s" % sys.exc_info, flush=True)
+    sys.exit(1)
