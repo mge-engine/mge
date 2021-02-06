4a16b8fbf3b66dd747bcce6a9dacb76b11a6c4e9
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-06 16:46:36 +0100

    Move vcpkg stuff to script

---
+++ tools/ci/vcpkg.py
@@ -0,0 +1,32 @@
+
+import os
+import sys
+
+version = 1
+vcpkg_githash = "70422b96724101d67e88923539cf2ea2c880c39a"
+vcpkg_packages = [
+    "gtest", "boost-preprocessor", "boost-filesystem",
+    "boost-circular-buffer", "boost-lexical-cast", "boost-property-tree"
+]
+
+
+def install_vcpkg():
+    pass
+
+
+if os.path.exists("vcpkg") and os.path.isdir("vcpkg"):
+    print("vcpkg path exists")
+else:
+    install_vcpkg()
+
+sys.exit(1)
+
+#   - cmd: git clone https://github.com/mge-engine/vcpkg.git
+#  - cmd: cd vcpkg
+#  - cmd: git checkout 70422b96724101d67e88923539cf2ea2c880c39a
+#  - cmd: .\bootstrap-vcpkg.bat -disableMetrics
+#  - cmd: .\vcpkg.exe install --triplet x64-windows gtest
+#  - cmd: .\vcpkg.exe install --triplet x64-windows boost-preprocessor boost-filesystem boost-circular-buffer
+#  - cmd: .\vcpkg.exe install --triplet x64-windows boost-lexical-cast boost-property-tree
+#  - cmd: cd ..
+# advice.detachedHead
