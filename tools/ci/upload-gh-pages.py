8b4fe8bea01a44c36880f7927a2873fedb1607f9
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-07 09:59:32 +0100

    Update gh pages script / update gh-pages

--- tools/ci/upload-gh-pages.py
+++ tools/ci/upload-gh-pages.py
@@ -1,3 +1,39 @@
 import sys
+import os
+
+upload_branches = ["main"]
+
+
+def upload_enabled():
+    try:
+        env = os.environ.copy()
+        if env["APPVEYOR_REPO_BRANCH"] in upload_branches:
+            print("Branch is %s, upload enabled" %
+                  (env["APPVEYOR_REPO_BRANCH"]))
+            return True
+    except:
+        pass
+    try:
+        env = os.environ.copy()
+        if "update gh-pages" in env["APPVEYOR_REPO_COMMIT_MESSAGE"]:
+            print("Updating gh-pages due to special commit message")
+            return True
+    except:
+        pass
+    return False
+
+
+def upload():
+    pass
+
+
+try:
+    if upload_enabled():
+        upload()
+    else:
+        print("No upload to gh-pages from this build")
+except:
+    print("Exception %s" % str(sys.exc_info()))
+    sys.exit(1)
 
 sys.exit(0)
