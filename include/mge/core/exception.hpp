919e09fd06a937c7f3e475c60748b60af3fa4a74
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-13 21:55:59 +0100

    Create standard exceptions
    Fixes #98

--- include/mge/core/exception.hpp
+++ include/mge/core/exception.hpp
@@ -13,7 +13,6 @@
 #include <type_traits>
 #include <typeindex>
 #include <typeinfo>
-
 namespace mge {
 
     /**
---
+++ src/mge/core/stdexceptions.cpp
@@ -0,0 +1,17 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "mge/core/stdexceptions.hpp"
+namespace mge {
+
+    MGE_DEFINE_EXCEPTION_CLASS(illegal_state)
+    MGE_DEFINE_EXCEPTION_CLASS(illegal_argument)
+    MGE_DEFINE_EXCEPTION_CLASS(out_of_range)
+    MGE_DEFINE_EXCEPTION_CLASS(duplicate_element)
+    MGE_DEFINE_EXCEPTION_CLASS(bad_cast)
+    MGE_DEFINE_EXCEPTION_CLASS(no_such_element)
+    MGE_DEFINE_EXCEPTION_CLASS(runtime_exception)
+    MGE_DEFINE_EXCEPTION_CLASS(not_yet_implemented)
+    MGE_DEFINE_EXCEPTION_CLASS(null_pointer)
+
+}
