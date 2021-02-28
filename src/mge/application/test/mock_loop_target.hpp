80498c6e54e33ac45ec607c37871fca7a32ec683
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-02-28 10:28:49 +0100

    simple loop test

---
+++ src/mge/application/test/mock_loop_target.hpp
@@ -0,0 +1,17 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "mge/application/loop_target.hpp"
+#include "test/googlemock.hpp"
+
+class MOCK_loop_target : public mge::loop_target
+{
+public:
+    MOCK_loop_target()          = default;
+    virtual ~MOCK_loop_target() = default;
+
+    MOCK_METHOD(bool, is_quit, (), (const));
+    MOCK_METHOD(void, input, ());
+    MOCK_METHOD(void, update, (double));
+    MOCK_METHOD(void, present, (double));
+};
---
+++ src/mge/application/test/test_simple_loop.cpp
@@ -0,0 +1,40 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "mge/application/loop.hpp"
+#include "mge/core/component.hpp"
+#include "mock_loop_target.hpp"
+
+using namespace testing;
+
+TEST(simple_loop, create)
+{
+    auto l = mge::loop::create("simple");
+    EXPECT_TRUE(l);
+}
+
+TEST(simple_loop, no_loop_is_quit)
+{
+    auto             l = mge::loop::create("simple");
+    MOCK_loop_target t;
+    EXPECT_CALL(t, is_quit()).Times(1).WillOnce(Return(true));
+    l->run(t);
+}
+
+TEST(simple_loop, one_successful_loop)
+{
+    auto             l = mge::loop::create("simple");
+    MOCK_loop_target t;
+    EXPECT_CALL(t, is_quit())
+        .Times(2)
+        .WillOnce(Return(false))
+        .WillOnce(Return(true));
+    {
+        InSequence s;
+
+        EXPECT_CALL(t, input());
+        EXPECT_CALL(t, update(_));
+        EXPECT_CALL(t, present(0.0));
+    }
+    l->run(t);
+}
