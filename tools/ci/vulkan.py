6d44ac69fb3ef54794f759fe123dbc9e7684c549
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-03-26 23:50:09 +0100

    more tests

--- tools/ci/vulkan.py
+++ tools/ci/vulkan.py
@@ -13,18 +13,24 @@ def vulkan_installer_exists():
     return os.path.exists(vulkan_installer)
 
 
-def install_vulkan():
+def download_vulkan():
     if os.path.exists(vulkan_installer_cache_dir):
         shutil.rmtree(vulkan_installer_cache_dir)
     os.mkdir('vulkan-installer')
     print("Downloading vulkan installer", flush=True)
     subprocess.run(["curl", "--show-error", "--output",
                     '%s\VulkanSDK-%s-Installer.exe' % (vulkan_installer_cache_dir, vulkan_version), "https://sdk.lunarg.com/sdk/download/%s/windows/VulkanSDK-%s-Installer.exe?u=" % (vulkan_version, vulkan_version)], )
+
+
+def install_vulkan():
     print("Run vulkan installer", flush=True)
     subprocess.run([vulkan_installer, "/S"])
 
 
 try:
+    if not vulkan_installer_exists():
+        download_vulkan()
+
     install_vulkan()
 except:
     print("Exception %s" % str(sys.exc_info()), flush=True)
