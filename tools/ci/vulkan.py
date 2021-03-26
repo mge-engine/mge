eb2b92ff97be07eff1bd4125ba87b8f7776e984b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-03-26 23:34:37 +0100

    run installer

--- tools/ci/vulkan.py
+++ tools/ci/vulkan.py
@@ -5,16 +5,22 @@ import shutil
 
 vulkan_version = "1.2.170.0"
 vulkan_installer_cache_dir = 'vulkan-installer'
+vulkan_installer = '%s\VulkanSDK-%s-Installer.exe' % (
+    vulkan_installer_cache_dir, vulkan_version)
 
-# if not exist $(VULKAN_INSTALLER_CACHE_FOLDER_FOR_CMD)\VulkanSDK-1.1.101.0-Installer.exe
+
+def vulkan_installer_exists():
+    return os.path.exists(vulkan_installer)
 
 
 def install_vulkan():
     if os.path.exists(vulkan_installer_cache_dir):
         shutil.rmtree(vulkan_installer_cache_dir)
     os.mkdir('vulkan-installer')
+    print("Downloading vulkan installer", flush=True)
     subprocess.run(["curl", "--show-error", "--output",
                     '%s\VulkanSDK-%s-Installer.exe' % (vulkan_installer_cache_dir, vulkan_version), "https://sdk.lunarg.com/sdk/download/%s/windows/VulkanSDK-%s-Installer.exe?u=" % (vulkan_version, vulkan_version)], )
+    subprocess.run([vulkan_installer, "-?"])
 
 
 try:
