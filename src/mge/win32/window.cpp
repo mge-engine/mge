96742263656ad1bf1f020fde0a252ddbad201663
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-03-28 16:05:24 +0200

    WIP: win32 window

---
+++ src/mge/win32/window.cpp
@@ -0,0 +1,47 @@
+#include "mge/win32/window.hpp"
+#include "mge/core/zero_memory.hpp"
+
+#define MGE_CLASS_NAME  ((LPCWSTR)L"mge")
+#define WM_WANT_DESTROY (WM_USER + 1)
+
+namespace mge {
+    namespace win32 {
+
+        window::window(const mge::extent &extent, const window_options &options)
+            : mge::window(extent, options), m_hwnd(0)
+        {}
+
+        window::~window() {}
+
+        volatile bool s_window_class_created;
+
+        void window::create_window_class()
+        {
+            WNDCLASSEXW window_class;
+            HINSTANCE   module_handle;
+
+            module_handle              = GetModuleHandle(NULL);
+            window_class.cbSize        = sizeof(WNDCLASSEX);
+            window_class.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+            window_class.lpfnWndProc   = (WNDPROC)wndproc;
+            window_class.cbWndExtra    = sizeof(void *);
+            window_class.hInstance     = module_handle;
+            window_class.hCursor       = LoadCursor(NULL, IDC_ARROW);
+            window_class.lpszClassName = MGE_CLASS_NAME;
+            window_class.hIcon = LoadIcon(module_handle, IDI_APPLICATION);
+            window_class.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+            window_class.hIconSm = LoadIcon(module_handle, IDI_APPLICATION);
+            if (!RegisterClassExW(&window_class)) {
+                //
+            }
+            s_window_class_created = true;
+        }
+
+        LRESULT CALLBACK window::wndproc(HWND hwnd, UINT umsg, WPARAM wparam,
+                                         LPARAM lparam)
+        {
+            return 0;
+        }
+
+    } // namespace win32
+} // namespace mge
--- src/mge/win32/window.hpp
+++ src/mge/win32/window.hpp
@@ -1,3 +1,29 @@
 // mge - Modern Game Engine
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
+
+#include "mge/graphics/window.hpp"
+
+#include <windows.h>
+
+namespace mge {
+    namespace win32 {
+
+        class window : public mge::window
+        {
+        public:
+            window(const mge::extent &extent, const window_options &options);
+            ~window();
+
+            HWND hwnd() const noexcept { return m_hwnd; }
+
+        private:
+            static LRESULT CALLBACK wndproc(HWND hwnd, UINT umsg, WPARAM wparam,
+                                            LPARAM lparam);
+            static void             create_window_class();
+
+            HWND m_hwnd;
+        };
+
+    } // namespace win32
+} // namespace mge
