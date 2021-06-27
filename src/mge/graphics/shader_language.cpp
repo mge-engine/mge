df358a05ba92d3ca6317f80f7501a89380521910
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-06-27 14:12:21 +0200

    dev continued_ shader language

--- src/mge/graphics/shader_language.cpp
+++ src/mge/graphics/shader_language.cpp
@@ -1,4 +1,5 @@
 #include "mge/graphics/shader_language.hpp"
+#include <ostream>
 
 namespace mge {
     shader_language::shader_language(std::string_view        name,
@@ -7,4 +8,30 @@ namespace mge {
     {}
 
     shader_language::~shader_language() {}
+
+    std::string_view shader_language::name() const { return m_name; }
+
+    const semantic_version &shader_language::version() const
+    {
+        return m_version;
+    }
+
+    bool shader_language::operator==(const shader_language &l) const
+    {
+        return m_name == l.m_name && m_version == l.m_version;
+    }
+
+    bool shader_language::compatible(const shader_language &other) const
+    {
+        if (m_name == other.m_name) {
+            return m_version <= other.m_version;
+        } else {
+            return false;
+        }
+    }
+
+    std::ostream &operator<<(std::ostream &os, const shader_language &l)
+    {
+        return os << l.name() << " " << l.version();
+    }
 } // namespace mge
--- src/mge/graphics/shader_language.hpp
+++ src/mge/graphics/shader_language.hpp
@@ -2,19 +2,67 @@
 #include "mge/core/semantic_version.hpp"
 #include "mge/graphics/dllexport.hpp"
 
+#include <iosfwd>
 #include <string>
 #include <string_view>
 
 namespace mge {
+
+    /**
+     * @brief A shader language.
+     *
+     * A shader language consists of a name and a version.
+     *
+     */
     class MGEGRAPHICS_EXPORT shader_language
     {
     public:
+        /**
+         * @brief Create a shader language object.
+         *
+         * @param name      shader language name
+         * @param version   shader language version
+         */
         shader_language(std::string_view name, const semantic_version &version);
+
         ~shader_language();
+
+        /**
+         * @brief Get shader language name.
+         *
+         * @return shader language name
+         */
+        std::string_view name() const;
+
+        /**
+         * @brief Get shader language version.
+         *
+         * @return shader language version
+         */
+        const semantic_version &version() const;
+
+        /**
+         * @brief Comparison operator.
+         *
+         * @param l compared language
+         * @return @c true if equal
+         */
+        bool operator==(const shader_language &l) const;
+
+        /**
+         * @brief Return this language is compatible with the other
+         *
+         * @param other
+         * @return @c true if compatible, i.e. same name, and this has less or
+         * equal version.
+         */
+        bool compatible(const shader_language &other) const;
 
     private:
         std::string      m_name;
         semantic_version m_version;
     };
 
+    MGEGRAPHICS_EXPORT std::ostream &operator<<(std::ostream &         os,
+                                                const shader_language &l);
 } // namespace mge
