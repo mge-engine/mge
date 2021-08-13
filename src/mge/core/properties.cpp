e3896ca4af224a0e20af2c1d9448d34499f81908
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-08-13 18:28:29 +0200

    Dev continued: properties

---
+++ src/mge/core/properties.cpp
@@ -0,0 +1,74 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "mge/core/properties.hpp"
+#include "mge/config.hpp"
+
+namespace mge {
+    properties::properties() {}
+
+    properties::properties(const properties& p)
+        : m_data(p.m_data)
+    {}
+
+    properties::properties(properties&& p)
+        : m_data(std::move(p.m_data))
+    {}
+
+    properties::properties(const input_stream_ref& input) { load(input->istream()); }
+
+    properties& properties::operator=(const properties& p)
+    {
+        m_data = p.m_data;
+        return *this;
+    }
+
+    properties& properties::operator=(properties&& p)
+    {
+        m_data = std::move(p.m_data);
+        return *this;
+    }
+
+    void properties::clear() { m_data.clear(); }
+
+    void properties::erase(std::string_view key)
+    {
+        m_data.erase(std::string(key.begin(), key.end()));
+    }
+
+    void properties::erase(const std::string& key) { m_data.erase(key); }
+
+    void properties::load(std::istream& s)
+    {
+        std::unordered_map<std::string, std::string> tmp_data;
+
+        std::string line;
+
+        while (!s.eof()) {
+            std::getline(s, line);
+            // skip completely empty lines
+            if (line.empty()) {
+                continue;
+            }
+            auto eqpos = line.find_first_of('=', 0);
+            if (eqpos == std::string::npos) {
+                MGE_THROW(runtime_exception) << "Invalid property file line '" << line << "' found";
+            }
+            std::string_view key(&line[0], eqpos);
+            std::string_view value(&line[0] + eqpos + 1, line.size() - eqpos - 1);
+            auto             value_end = value.end();
+// strip spurious \r
+#ifdef MGE_OS_WINDOWS
+            if (!value.empty()) {
+                if (*value.rbegin() == '\r') {
+                    --value_end;
+                }
+            }
+#endif
+            tmp_data[std::string(key.begin(), key.end())] = std::string(value.begin(), value_end);
+        }
+        std::swap(m_data, tmp_data);
+        return;
+    }
+
+} // namespace mge
--- src/mge/core/properties.hpp
+++ src/mge/core/properties.hpp
@@ -2,13 +2,12 @@
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
 #pragma once
+#include "boost/boost_lexical_cast.hpp"
 #include "mge/core/dllexport.hpp"
-//#include "mge/core/input_stream.hpp"
-//#include "mge/core/lexical_cast.hpp"
+#include "mge/core/input_stream.hpp"
 #include "mge/core/memory.hpp"
 #include "mge/core/stdexceptions.hpp"
-#include "mge/core/string_pool.hpp"
-
+#include <string_view>
 #include <unordered_map>
 
 namespace mge {
@@ -26,16 +25,19 @@ namespace mge {
         properties();
         properties(const properties& p);
         properties(properties&& p);
-        // properties(const input_stream_ref& input);
+        properties(const input_stream_ref& input);
 
         properties& operator=(const properties& p);
         properties& operator=(properties&& p);
 
-        bool exists(const char* key) const { return m_data.find(key) != m_data.end(); }
+        bool exists(std::string_view key) const
+        {
+            return m_data.find(std::string(key.begin(), key.end())) != m_data.end();
+        }
 
         bool exists(const std::string& key) const { return m_data.find(key) != m_data.end(); }
 
-        template <typename T> T get(const char* key) const
+        template <typename T> T get(std::string_view key) const
         {
             if (key == nullptr) {
                 MGE_THROW(null_pointer) << "Argument 'key' must not be null";
@@ -43,12 +45,12 @@ namespace mge {
 
             auto it = m_data.find(key);
             if (it != m_data.end()) {
-                return lexical_cast<T>(it->second);
+                return boost::lexical_cast<T>(it->second);
             }
             MGE_THROW(no_such_element) << "No property '" << key << "' found";
         }
 
-        template <typename T, typename D> T get(const char* key, const D& default_value) const
+        template <typename T, typename D> T get(std::string_view key, const D& default_value) const
         {
             if (key == nullptr) {
                 return default_value;
@@ -56,7 +58,7 @@ namespace mge {
 
             auto it = m_data.find(key);
             if (it != m_data.end()) {
-                return lexical_cast<T>(it->second);
+                return boost::lexical_cast<T>(it->second);
             } else {
                 return default_value;
             }
@@ -66,7 +68,7 @@ namespace mge {
         {
             auto it = m_data.find(key);
             if (it != m_data.end()) {
-                return lexical_cast<T>(it->second);
+                return boost::lexical_cast<T>(it->second);
             }
             MGE_THROW(no_such_element) << "No property '" << key << "' found";
         }
@@ -76,13 +78,13 @@ namespace mge {
         {
             auto it = m_data.find(key);
             if (it != m_data.end()) {
-                return lexical_cast<T>(it->second);
+                return boost::lexical_cast<T>(it->second);
             } else {
                 return default_value;
             }
         }
 
-        template <typename T> inline void put(const char* key, const T& value)
+        template <typename T> inline void put(std::string_view key, const T& value)
         {
             std::stringstream ss;
             ss << value;
@@ -96,7 +98,7 @@ namespace mge {
             m_data[key] = ss.str();
         }
 
-        void erase(const char* key);
+        void erase(std::string_view key);
         void erase(const std::string& key);
 
         void clear();
@@ -104,8 +106,7 @@ namespace mge {
         void load(std::istream& s);
 
     private:
-        string_pool                                            m_pool;
-        std::unordered_map<std::string_view, std::string_view> m_data;
+        std::unordered_map<std::string, std::string> m_data;
     };
 
 } // namespace mge
