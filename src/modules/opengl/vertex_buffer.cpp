98d6704f285ba516e3f4edb4caad223a851de456
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-08-17 19:50:52 +0200

    Formatting

--- src/modules/opengl/vertex_buffer.cpp
+++ src/modules/opengl/vertex_buffer.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #include "vertex_buffer.hpp"
 #include "error.hpp"
 #include "mge/core/checked_cast.hpp"
