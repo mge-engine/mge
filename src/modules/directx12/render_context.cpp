5fe4532cf935859a1e0f352178ef6278bc86ba94
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-08-21 22:37:09 +0200

    dx12: adapter selection

--- src/modules/directx12/render_context.cpp
+++ src/modules/directx12/render_context.cpp
@@ -21,13 +21,15 @@ namespace mge::dx12 {
 
     mge::com_ptr<IDXGIAdapter4> render_context::get_adapter()
     {
-        UINT factory_flags = 0;
+        HRESULT rc = S_OK;
+        UINT    factory_flags = 0;
         if (m_render_system.debug()) {
             factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
         }
         MGE_DEBUG_TRACE(DX12) << "Create DXGI Factory";
         mge::com_ptr<IDXGIFactory4> factory;
-        auto rc = CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&factory));
+        rc = CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&factory));
+        CHECK_HRESULT(rc, , CreateDXGIFactory2);
         ::mge::dx12::error::check_hresult(rc,
                                           __FILE__,
                                           __LINE__,
@@ -36,9 +38,44 @@ namespace mge::dx12 {
         if (m_render_system.warp()) {
             mge::com_ptr<IDXGIAdapter1> adapter1;
             mge::com_ptr<IDXGIAdapter4> adapter4;
+            rc = factory->EnumWarpAdapter(IID_PPV_ARGS(&adapter1));
+            CHECK_HRESULT(rc, IDXGIFactory4, EnumWarpAdapter);
+            rc = adapter1.As(&adapter4);
+            CHECK_HRESULT(rc, com_ptr, As<IDXGIAdapter4>);
             return adapter4;
         } else {
+            size_t max_dedicated_video_mem = 0;
+
+            mge::com_ptr<IDXGIAdapter1> adapter1;
             mge::com_ptr<IDXGIAdapter4> adapter4;
+            for (uint32_t i = 0;
+                 factory->EnumAdapters1(i, &adapter1) != DXGI_ERROR_NOT_FOUND;
+                 ++i) {
+                DXGI_ADAPTER_DESC1 desc_adapter1;
+
+                rc = adapter1->GetDesc1(&desc_adapter1);
+                CHECK_HRESULT(rc, IDXGIAdapter1, GetDesc1);
+
+                if ((desc_adapter1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0) {
+                    if (SUCCEEDED(D3D12CreateDevice(adapter1.Get(),
+                                                    D3D_FEATURE_LEVEL_11_0,
+                                                    __uuidof(ID3D12Device),
+                                                    nullptr))) {
+                        if (desc_adapter1.DedicatedVideoMemory >
+                            max_dedicated_video_mem) {
+                            max_dedicated_video_mem =
+                                desc_adapter1.DedicatedVideoMemory;
+                            rc = adapter1.As(&adapter4);
+                            CHECK_HRESULT(rc, com_ptr, As<IDXGIAdapter4>);
+                        }
+                    }
+                }
+            }
+
+            if (!adapter4.Get()) {
+                MGE_THROW(dx12::error) << "No suitable hardware adapter found";
+            }
+
             return adapter4;
         }
     }
