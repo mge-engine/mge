2cacc70849566a7a286ee7aca305ed3562e80e7d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-08-22 20:44:49 +0200

    Fix copyright

--- src/modules/directx12/render_context.cpp
+++ src/modules/directx12/render_context.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #include "render_context.hpp"
 #include "error.hpp"
 #include "mge/core/array_size.hpp"
