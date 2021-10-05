7dd595357da55e0ffdbcef6901f4299688482d87
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-05 10:08:48 +0200

    Improve trace helper scripts

--- tools/disable_trace.cmd
+++ tools/disable_trace.cmd
@@ -1,1 +1,4 @@
-echo Disable trace to console
+@echo off
+@echo Disable trace to console
+SET MGE_TRACE_TO_STDOUT=
+SET MGE_TRACE_ENABLED=
--- tools/enable_trace.cmd
+++ tools/enable_trace.cmd
@@ -1,3 +1,4 @@
-echo Enable trace to console
+@echo off
+@echo Enable trace to console
 SET MGE_TRACE_TO_STDOUT=1
 SET MGE_TRACE_ENABLED=1
