0b38be4f2dfab79a46410d969a9d464235f342e6
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-10 15:40:37 +0200

    Fix copyright

--- src/mge/application/fixed_step_loop.cpp
+++ src/mge/application/fixed_step_loop.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #include "mge/application/loop.hpp"
 #include "mge/core/parameter.hpp"
 #include "mge/core/trace.hpp"
