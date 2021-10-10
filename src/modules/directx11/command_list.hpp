d587ca6bbcee7307417c0a8767d0bee7ed075f8b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-10 15:40:50 +0200

    Fix copyright

--- src/modules/directx11/command_list.hpp
+++ src/modules/directx11/command_list.hpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #pragma once
 #include "mge/graphics/command_list.hpp"
 #include "mge/win32/com_unique_ptr.hpp"
