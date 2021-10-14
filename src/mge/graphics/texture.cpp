313339eaa09329a8ace514a52e0b706806e59321
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-14 21:48:14 +0200

    Scaffold: texture

---
+++ src/mge/graphics/texture.cpp
@@ -0,0 +1,8 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "texture.hpp"
+
+namespace mge {
+    texture::~texture() {}
+} // namespace mge
---
+++ src/mge/graphics/texture.hpp
@@ -0,0 +1,18 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/graphics/context_object.hpp"
+
+namespace mge {
+
+    class MGEGRAPHICS_EXPORT texture : public context_object
+    {
+    protected:
+        texture(render_context&);
+
+    public:
+        virtual ~texture();
+    };
+
+} // namespace mge
