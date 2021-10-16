ef1e866ca993e71a42835e0c196bb247dd1c1df3
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-17 00:49:31 +0200

    Fix copyright

--- src/mge/asset/test/test_asset_type.cpp
+++ src/mge/asset/test/test_asset_type.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #include "mge/asset/asset_type.hpp"
 #include "test/googletest.hpp"
 #include "test/test_stream_op.hpp"
--- src/mge/script/script_context.hpp
+++ src/mge/script/script_context.hpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
 #pragma once
 #include "mge/core/noncopyable.hpp"
 #include "mge/script/dllexport.hpp"
