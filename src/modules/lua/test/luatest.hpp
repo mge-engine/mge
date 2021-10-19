6e58c55cd1db3269a840b645fc84f380d217b2d4
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-10-19 18:28:09 +0200

    lua context

--- src/modules/lua/test/luatest.hpp
+++ src/modules/lua/test/luatest.hpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
 #pragma once
+#include "mge/script/script_context.hpp"
 #include "mge/script/script_engine.hpp"
 #include "test/applicationtest.hpp"
 #include "test/googletest.hpp"
--- src/modules/lua/test/test_context.cpp
+++ src/modules/lua/test/test_context.cpp
@@ -9,4 +9,31 @@ namespace mge::lua {
 
     TEST_F(test_context, create) { auto context = engine->create_context(); }
 
+    TEST_F(test_context, simple_eval)
+    {
+        auto context = engine->create_context();
+        context->eval("i = 1\n"
+                      "while i < 3 do\n"
+                      "  print(i)\n"
+                      "  i = i + 1\n"
+                      "end");
+    }
+
+    TEST_F(test_context, simple_eval_throws_on_syntax_error)
+    {
+        auto ctx = engine->create_context();
+        try {
+            ctx->eval("i = 1\n"
+                      "while i < 3 <3 >5 do\n"
+                      "  print(i)\n"
+                      "  i = i + 1\n"
+                      "end");
+            FAIL() << "Exception expected";
+        } catch (const mge::exception& ex) {
+            EXPECT_TRUE(
+                strstr(ex.what(), "attempt to compare boolean with number") !=
+                nullptr);
+        }
+    }
+
 } // namespace mge::lua
