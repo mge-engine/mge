f9d502da9ee91f083dc95b46ec4a579971913d63
Alexander Schroeder <schroeder.alexander@googlemail.com>
2021-12-18 13:32:16 +0100

    call context

---
+++ src/modules/python/python_call_context.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include "mge/script/call_context.hpp"
+#include "python.hpp"
+
+namespace mge::python {
+    class python_call_context : public mge::script::call_context
+    {
+    public:
+        python_call_context(PyObject* args);
+        ~python_call_context();
+
+    protected:
+        bool           bool_parameter(size_t position) override;
+        char           char_parameter(size_t position) override;
+        unsigned char  unsigned_char_parameter(size_t position) override;
+        short          short_parameter(size_t position) override;
+        unsigned short unsigned_short_parameter(size_t position) override;
+        int            int_parameter(size_t position) override;
+        unsigned int   unsigned_int_parameter(size_t position) override;
+        int64_t        long_parameter(size_t position) override;
+        uint64_t       unsigned_long_parameter(size_t position) override;
+        float          float_parameter(size_t position) override;
+        double         double_parameter(size_t position) override;
+        std::string    string_parameter(size_t position) override;
+
+        void store_bool_result(bool result) override;
+        void store_char_result(char result) override;
+        void store_signed_char_result(signed char result) override;
+        void store_unsigned_char_result(unsigned char result) override;
+        void store_short_result(short result) override;
+        void store_unsigned_short_result(unsigned short result) override;
+        void store_int_result(int result) override;
+        void store_unsigned_int_result(unsigned int result) override;
+        void store_long_result(int64_t result) override;
+        void store_unsigned_long_result(uint64_t result) override;
+        void store_float_result(float result) override;
+        void store_double_result(double result) override;
+        void store_string_result(const std::string& result) override;
+
+    private:
+        PyObject* parameter(size_t position);
+        void      store(PyObject* object);
+
+        PyObject* m_args;
+        PyObject* m_result;
+    };
+} // namespace mge::python
