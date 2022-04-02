3790b41e786255ddea215148187e97538e85d4fc
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-04-02 18:50:45 +0200

    wip: python module

--- src/modules/python/python_module.cpp
+++ src/modules/python/python_module.cpp
@@ -1,3 +1,19 @@
 #include "python_module.hpp"
+#include "python_error.hpp"
 
-namespace mge::python {}
+namespace mge::python {
+    python_module::python_module(const mge::script::module& m)
+        : m_py_module(nullptr)
+        , m_module(m)
+    {
+        if (m_module.is_root()) {
+            m_py_module = PyImport_AddModule("__main__");
+        } else {
+            m_py_module = PyImport_AddModule(m.name().c_str());
+        }
+        error::check_error();
+    }
+
+    python_module::~python_module() { Py_XDECREF(m_py_module); }
+
+} // namespace mge::python
--- src/modules/python/python_module.hpp
+++ src/modules/python/python_module.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include "mge/script/module.hpp"
+#include "python.hpp"
 
 namespace mge::python {
     class python_module
-    {};
+    {
+    public:
+        python_module(const mge::script::module& m);
+        ~python_module();
+
+    private:
+        PyObject*           m_py_module;
+        mge::script::module m_module;
+    };
 } // namespace mge::python
