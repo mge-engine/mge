19a42a70d4a5fe2fa56079a47ec4e36a9270910f
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-05-01 17:21:08 +0200

    finalize line editor

--- src/mge/core/line_editor.cpp
+++ src/mge/core/line_editor.cpp
@@ -14,15 +14,31 @@ namespace mge {
         }
     }
 
+    line_editor::line_editor(const std::string& prompt)
+    {
+        if (++s_instances > 1) {
+            MGE_THROW(illegal_state)
+                << "Only one line editor can be instantiated at same time";
+        }
+        m_prompt = prompt;
+    }
+
     line_editor::~line_editor() { --s_instances; }
 
     std::string line_editor::line()
     {
-        char* l = linenoise(">");
+        char* l = linenoise(m_prompt.c_str());
 
         std::string result(l);
         free(l);
 
         return result;
+    }
+
+    const std::string& line_editor::prompt() const { return m_prompt; }
+
+    void line_editor::set_prompt(const std::string& prompt)
+    {
+        m_prompt = prompt;
     }
 } // namespace mge
