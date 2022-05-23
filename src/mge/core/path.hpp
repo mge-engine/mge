5d09acdf49187485f6371f9148bb024176b2aacb
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-05-23 07:17:53 +0200

    Add path alias

---
+++ src/mge/core/path.hpp
@@ -0,0 +1,16 @@
+// mge - Modern Game Engine
+// Copyright (c) 2018 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/config.hpp"
+#include "mge/core/dllexport.hpp"
+#include <filesystem>
+#include <iosfwd>
+#include <string>
+#include <vector>
+
+namespace mge {
+    using std::filesystem::canonical;
+    using std::filesystem::path;
+    using std::filesystem::weakly_canonical;
+} // namespace mge
