dfa7b981d376d71eee8b27f7a3303905c3e25f3d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-08-14 21:47:10 +0200

    check gist for signature

--- src/mge/script/test/test_signature.cpp
+++ src/mge/script/test/test_signature.cpp
@@ -1,5 +1,8 @@
+#include "mge/core/gist.hpp"
 #include "mge/script/signature.hpp"
 #include "test/googletest.hpp"
+
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -9,3 +12,13 @@ namespace mge::script {
         signature s = signature::create<int>();
     }
 } // namespace mge::script
+
+namespace mge::foo {
+    TEST(signature, gist)
+    {
+        mge::script::signature s = mge::script::signature::create<int, float>();
+        std::stringstream      ss;
+        ss << gist(s);
+        EXPECT_EQ(ss.str(), "(int, float)");
+    }
+} // namespace mge::foo
