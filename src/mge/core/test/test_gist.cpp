79e66c06ea39cd79b543a24dd34b4248f30f1b71
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-08-14 13:46:28 +0200

    basic gist tests

---
+++ src/mge/core/test/test_gist.cpp
@@ -0,0 +1,28 @@
+// mge - Modern Game Engine
+// Copyright (c) 2021 by Alexander Schroeder
+// All rights reserved.
+#include "mge/core/gist.hpp"
+#include "test/googletest.hpp"
+
+#include <sstream>
+
+using namespace mge;
+
+TEST(gist, pod)
+{
+    std::stringstream ss;
+    ss << gist(2);
+    EXPECT_EQ("2", ss.str());
+}
+
+struct no_gist_op
+{};
+
+TEST(gist, no_gist_op)
+{
+    no_gist_op        val;
+    std::stringstream ss;
+    ss << gist(val);
+
+    EXPECT_TRUE(ss.str().starts_with("no_gist_op@"));
+}
