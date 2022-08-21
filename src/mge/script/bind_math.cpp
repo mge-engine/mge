53e179e6ef43cfeb9ee1a462c61b2f5003a88cee
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-08-21 10:06:35 +0200

    script: more bindings

--- src/mge/script/bind_math.cpp
+++ src/mge/script/bind_math.cpp
@@ -2,6 +2,8 @@
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
 #include "mge/math/vec2.hpp"
+#include "mge/math/vec3.hpp"
+#include "mge/math/vec4.hpp"
 
 #include "mge/script/function.hpp"
 #include "mge/script/module.hpp"
@@ -17,8 +19,26 @@ namespace mge::script {
         void bind()
         {
             module("mge")(type<mge::fvec2>("fvec2")
+                              .constructor()
+                              .copy_constructor()
+                              .constructor<float, float>()
                               .field("x", &mge::fvec2::x)
                               .field("y", &mge::fvec2::y));
+            module("mge")(type<mge::fvec3>("fvec3")
+                              .constructor()
+                              .copy_constructor()
+                              .constructor<float, float, float>()
+                              .field("x", &mge::fvec3::x)
+                              .field("y", &mge::fvec3::y)
+                              .field("z", &mge::fvec3::z));
+            module("mge")(type<mge::fvec4>("fvec4")
+                              .constructor()
+                              .copy_constructor()
+                              .constructor<float, float, float, float>()
+                              .field("x", &mge::fvec4::x)
+                              .field("y", &mge::fvec4::y)
+                              .field("z", &mge::fvec4::z)
+                              .field("w", &mge::fvec4::w));
         }
     };
 
