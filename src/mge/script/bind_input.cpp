789392eb7e7052c733dbe88d197526342668b21b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-09-01 23:55:36 +0200

    wip: input binding

--- src/mge/script/bind_input.cpp
+++ src/mge/script/bind_input.cpp
@@ -1,10 +1,13 @@
 // mge - Modern Game Engine
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
+#include "mge/input/input_handler.hpp"
+#include "mge/input/input_state.hpp"
 #include "mge/input/key.hpp"
 #include "mge/input/keyboard_state.hpp"
 #include "mge/input/modifier.hpp"
 #include "mge/input/mouse_action.hpp"
+#include "mge/input/mouse_state.hpp"
 
 #include "mge/script/function.hpp"
 #include "mge/script/module.hpp"
@@ -22,7 +25,69 @@ namespace mge::script {
         {
             module("mge")(
                 // module begin
-                type<mge::keyboard_state>("keyboard_state"),
+                type<mge::input_handler>("input_handler")(
+                    type<mge::input_handler::mouse_action_handler>(
+                        "mouse_action_handler"),
+                    type<mge::input_handler::mouse_move_handler>(
+                        "mouse_move_handler"),
+                    type<mge::input_handler::key_action_handler>(
+                        "key_action_handler"),
+                    type<mge::input_handler::character_handler>(
+                        "character_handler"))
+                    .method("set_key_action_handler",
+                            &mge::input_handler::set_key_action_handler)
+                    .method("set_character_handler",
+                            &mge::input_handler::set_character_handler),
+                type<mge::input_state>("input_state")
+                    .constructor()
+                    .method("keyboard",
+                            static_cast<const mge::keyboard_state& (
+                                mge::input_state::*)() const>(
+                                &mge::input_state::keyboard))
+                    .method("mouse",
+                            static_cast<const mge::mouse_state& (
+                                mge::input_state::*)() const>(
+                                &mge::input_state::mouse)),
+                type<mge::keyboard_state>("keyboard_state")
+                    .constructor()
+                    .method(
+                        "press",
+                        static_cast<void (mge::keyboard_state::*)(
+                            const mge::modifier&)>(&mge::keyboard_state::press))
+                    .method("release",
+                            static_cast<void (mge::keyboard_state::*)(
+                                const mge::modifier&)>(
+                                &mge::keyboard_state::release))
+                    .method("press",
+                            static_cast<void (mge::keyboard_state::*)(
+                                const mge::key&)>(&mge::keyboard_state::press))
+                    .method(
+                        "release",
+                        static_cast<void (mge::keyboard_state::*)(
+                            const mge::key&)>(&mge::keyboard_state::release))
+                    .method("pressed",
+                            static_cast<bool (mge::keyboard_state::*)(
+                                const mge::key&) const>(
+                                &mge::keyboard_state::pressed))
+                    .method("pressed",
+                            static_cast<bool (mge::keyboard_state::*)(
+                                const mge::modifier&) const>(
+                                &mge::keyboard_state::pressed))
+                    .method("pressed",
+                            static_cast<bool (mge::keyboard_state::*)(
+                                const mge::modifier&,
+                                const mge::key&) const>(
+                                &mge::keyboard_state::pressed))
+                    .method("current_modifier",
+                            &mge::keyboard_state::current_modifier),
+                type<mge::mouse_state>("mouse_state")
+                    .constructor()
+                    .method("press", &mge::mouse_state::press)
+                    .method("release", &mge::mouse_state::release)
+                    .method("move", &mge::mouse_state::move)
+                    .method("x", &mge::mouse_state::x)
+                    .method("y", &mge::mouse_state::y)
+                    .method("pressed", &mge::mouse_state::pressed),
                 type<mge::key>("key")
                     .enum_value("INVALID", mge::key::INVALID)
                     .enum_value("SPACE", mge::key::SPACE)
