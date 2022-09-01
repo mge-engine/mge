168c68895ab3a60190f71ceee10a4c988aee068d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-09-01 16:20:00 +0200

    wip: bind

--- src/mge/script/bind_input.cpp
+++ src/mge/script/bind_input.cpp
@@ -2,6 +2,9 @@
 // Copyright (c) 2021 by Alexander Schroeder
 // All rights reserved.
 #include "mge/input/key.hpp"
+#include "mge/input/keyboard_state.hpp"
+#include "mge/input/modifier.hpp"
+#include "mge/input/mouse_action.hpp"
 
 #include "mge/script/function.hpp"
 #include "mge/script/module.hpp"
@@ -17,8 +20,24 @@ namespace mge::script {
 
         void bind()
         {
-            module("mge")(type<mge::key>("key")
-                          // module end
+            module("mge")(
+                // module begin
+                type<mge::keyboard_state>("keyboard_state"),
+                type<mge::key>("key")
+                    .enum_value("INVALID", mge::key::INVALID)
+                    .enum_value("SPACE", mge::key::SPACE)
+                    .enum_value("ZERO", mge::key::ZERO),
+                type<mge::modifier>("modifier"),
+                type<mge::modifier_value>("modifier_value")
+                    .enum_value("CONTROL", mge::modifier_value::CONTROL)
+                    .enum_value("SHIFT", mge::modifier_value::SHIFT)
+                    .enum_value("ALT", mge::modifier_value::ALT),
+                type<mge::mouse_action>("mouse_action")
+                    .enum_value("PRESS", mge::mouse_action::PRESS)
+                    .enum_value("RELEASE", mge::mouse_action::RELEASE)
+                    .enum_value("DOUBLE_CLICK",
+                                mge::mouse_action::MOUSE_ACTION_DOUBLE_CLICK)
+                // module end
             );
         }
     };
