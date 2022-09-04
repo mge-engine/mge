adf3264be3fd7d204e1fa6786210209566259b59
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-09-04 11:18:55 +0200

    only need to store ref to type detail

--- src/modules/python/python_type.hpp
+++ src/modules/python/python_type.hpp
@@ -118,7 +118,7 @@ namespace mge::python {
         mutable std::unique_ptr<create_data>       m_create_data;
         mutable PyObject*                          m_python_type;
         python_context&                            m_context;
-        mge::script::type_details_ref              m_type;
+        const mge::script::type_details_ref&       m_type;
         std::vector<field>                         m_fields;
         std::map<size_t, std::vector<constructor>> m_constructors;
         destructor                                 m_destructor;
