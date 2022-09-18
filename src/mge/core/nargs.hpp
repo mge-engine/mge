fd948a9579209f54e16a9947aae60022f9549530
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-09-18 15:21:40 +0200

    is_empty macro

--- src/mge/core/nargs.hpp
+++ src/mge/core/nargs.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// see https://gustedt.wordpress.com/2010/06/08/detect-empty-macro-arguments/
+// for elaboration on how this works
+
 #define MGE_NARGS_SELECT(_0,                                                   \
                          _1,                                                   \
                          _2,                                                   \
@@ -35,6 +38,42 @@
                          N,                                                    \
                          ...)                                                  \
     N
+
+#define MGE_HAS_COMMA(...)                                                     \
+    MGE_NARGS_SELECT(__VA_ARGS__,                                              \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     1,                                                        \
+                     0)
+
 #define MGE_NARGS(...)                                                         \
     MGE_NARGS_SELECT(__VA_ARGS__,                                              \
                      32,                                                       \
@@ -70,3 +109,18 @@
                      2,                                                        \
                      1,                                                        \
                      0)
+
+#define MGE_TRIGGER_PARENTHESIS(...) ,
+
+#define MGE_IS_EMPTY(...)                                                      \
+    MGE_IS_EMPTY_2(                                                            \
+        MGE_HAS_COMMA(__VA_ARGS__),                                            \
+        MGE_HAS_COMMA(MGE_TRIGGER_PARENTHESIS __VA_ARGS__),                    \
+        MGE_HAS_COMMA(__VA_ARGS__(/*empty*/)),                                 \
+        MGE_HAS_COMMA(MGE_TRIGGER_PARENTHESIS __VA_ARGS__(/*empty*/)))
+
+#define MGE_IS_EMPTY_CASE_0001 ,
+#define MGE_IS_EMPTY_PASTE5(_0, _1, _2, _3, _4) _0##_1##_2##_3##_4
+
+#define MGE_IS_EMPTY_2(_0, _1, _2, _3)                                         \
+    MGE_HAS_COMMA(MGE_IS_EMPTY_PASTE5(MGE_IS_EMPTY_CASE_, _0, _1, _2, _3))
--- src/mge/core/test/test_nargs.cpp
+++ src/mge/core/test/test_nargs.cpp
@@ -21,3 +21,23 @@ TEST(nargs, without_args)
     auto n = MGE_NARGS();
     EXPECT_EQ(1, n);
 }
+
+TEST(nargs, has_comma)
+{
+    auto hc = MGE_HAS_COMMA(a);
+    EXPECT_EQ(0, hc);
+    auto hc2 = MGE_HAS_COMMA();
+    EXPECT_EQ(0, hc2);
+    auto hc3 = MGE_HAS_COMMA(a, b);
+    EXPECT_EQ(1, hc3);
+}
+
+TEST(nargs, is_empty)
+{
+    auto is_empty = MGE_IS_EMPTY(aaa);
+    auto is_empty2 = MGE_IS_EMPTY(aaa, bbb);
+    auto is_empty3 = MGE_IS_EMPTY();
+    EXPECT_EQ(0, is_empty);
+    EXPECT_EQ(0, is_empty2);
+    EXPECT_EQ(1, is_empty3);
+}
