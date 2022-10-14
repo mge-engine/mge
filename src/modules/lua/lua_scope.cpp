d0795d36155e7252421a2590953c3a904203d2ef
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-10-14 23:44:34 +0200

    lua scope

--- src/modules/lua/lua_scope.cpp
+++ src/modules/lua/lua_scope.cpp
@@ -17,6 +17,40 @@ namespace mge::lua {
         lua_pop(L, 1);
     }
 
-    scope::~scope() {}
+    scope::scope(lua_context&          context,
+                 const lua::scope_ref& parent,
+                 const char*           name)
+        : m_context(context)
+        , m_name(name)
+        , m_parent(parent)
+    {
+        auto L = m_context.lua_state();
+        m_parent->load();
+        lua_newtable(L);
+        lua_setfield(L, -2, name);
+        CHECK_CURRENT_STATUS(L);
+        lua_pop(L, 1);
+    }
+
+    void scope::load()
+    {
+        auto L = m_context.lua_state();
+        if (!m_parent) {
+            lua_getglobal(L, m_name);
+            CHECK_CURRENT_STATUS(L);
+        } else {
+            m_parent->load();
+            lua_getfield(L, -1, m_name);
+            CHECK_CURRENT_STATUS(L);
+            lua_remove(L, -2);
+        }
+    }
+
+    scope::~scope()
+    {
+        // do not remove any of the created instances
+        // creating them again will invoke the gc, and
+        // do the needful
+    }
 
 } // namespace mge::lua
