734f53abc91032d4db6140ba9ebf1d761298adfa
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-10-30 08:49:58 +0100

    contd: draw command

--- src/mge/graphics/draw_command.cpp
+++ src/mge/graphics/draw_command.cpp
@@ -4,7 +4,19 @@
 #include "mge/graphics/draw_command.hpp"
 
 namespace mge {
-    draw_command::draw_command() {}
+    draw_command::draw_command()
+        : m_topology(mge::topology::TRIANGLES)
+    {}
+
+    draw_command::draw_command(const pipeline_ref&      pipeline,
+                               const vertex_buffer_ref& vertices,
+                               const index_buffer_ref&  indices,
+                               mge::topology            t)
+        : m_pipeline(pipeline)
+        , m_vertices(vertices)
+        , m_indices(indices)
+        , m_topology(t)
+    {}
 
     draw_command::~draw_command() {}
 } // namespace mge
--- src/mge/graphics/draw_command.hpp
+++ src/mge/graphics/draw_command.hpp
@@ -4,6 +4,7 @@
 #pragma once
 #include "mge/graphics/dllexport.hpp"
 #include "mge/graphics/graphics_fwd.hpp"
+#include "mge/graphics/topology.hpp"
 namespace mge {
 
     /**
@@ -16,9 +17,51 @@ namespace mge {
          * @brief Construct empty draw command.
          */
         draw_command();
+
+        /**
+         * @brief Construct a new draw command.
+         *
+         * @param pipeline pipeline to be executed
+         * @param vertices vertices
+         * @param indices  indices
+         * @param t        vertices topology
+         */
+        draw_command(const pipeline_ref&      pipeline,
+                     const vertex_buffer_ref& vertices,
+                     const index_buffer_ref&  indices,
+                     topology                 t);
+
         ~draw_command();
 
+        /**
+         * @brief Access stored pipeline.
+         * @return pipeline
+         */
+        const pipeline_ref& pipeline() const { return m_pipeline; }
+        /**
+         * @brief Access stored vertex buffer.
+         * @return vertex buffer
+         */
+        const vertex_buffer_ref& vertices() const { return m_vertices; }
+        /**
+         * @brief Access stored index buffer.
+         *
+         * @return const index buffer
+         */
+        const index_buffer_ref& indices() const { return m_indices; }
+
+        /**
+         * @brief Access stored topology.
+         *
+         * @return mge::topology
+         */
+        mge::topology topology() const { return m_topology; }
+
     private:
+        const pipeline_ref      m_pipeline;
+        const vertex_buffer_ref m_vertices;
+        const index_buffer_ref  m_indices;
+        const mge::topology     m_topology;
     };
 
 } // namespace mge
