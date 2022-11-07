d37a7f29ef99a8107919c535cc91927e9658595d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2022-11-07 19:12:23 +0100

    Texture type

---
+++ src/mge/graphics/texture_type.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "mge/graphics/dllexport.hpp"
+
+namespace mge {
+    enum class MGEGRAPHICS_EXPORT texture_type
+    {
+        TYPE_1D,
+        TYPE_2D,
+        TYPE_3D
+    };
+}
