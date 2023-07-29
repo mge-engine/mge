2705c509fe3acb3a7082561544b4ac95d8740080
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-07-29 10:20:27 +0200

    Update vcpkg

--- tools/ci/vcpkg_version.py
+++ tools/ci/vcpkg_version.py
@@ -2,5 +2,5 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-version = "38"
-vcpkg_githash = "4c1734ba2a969b1e6daa0ac79bc46ead7324df90"
+version = "39"
+vcpkg_githash = "8b04a7bd93bef991818fc372bb83ce00ec1c1c16"
