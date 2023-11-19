e40c79d591649e9fce3fb43248f7474e8ab0fbc6
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-11-19 13:54:56 +0100

    Adapt appveyor build

--- tools/ci/vcpkg.py
+++ tools/ci/vcpkg.py
@@ -14,14 +14,6 @@ import vcpkg_version
 # -- vcpkg_packages     array of package names to install
 version = vcpkg_version.version
 vcpkg_githash = vcpkg_version.vcpkg_githash
-vcpkg_packages = [
-    "gtest", "boost-preprocessor", "boost-filesystem",
-    "boost-circular-buffer", "boost-lexical-cast", "boost-property-tree",
-    "boost-spirit", "boost-locale",
-    "vulkan", "glm", "lua", "python3",
-    "glslang"
-]
-
 
 def install_vcpkg():
     print("Cloning vcpkg git mirror", flush=True)
@@ -33,17 +25,6 @@ def install_vcpkg():
     print("Bootstrapping vcpkg", flush=True)
     subprocess.run([".\\bootstrap-vcpkg.bat", "-disableMetrics"],
                    cwd="vcpkg", shell=True)
-    # use x64-windows-release to force dynamic runtime
-    install_command = ["vcpkg.exe", "install",
-                       "--triplet", "x64-windows", "--editable"]
-    install_command.extend(vcpkg_packages)
-    print("Installing following packages:", flush=True)
-    for p in vcpkg_packages:
-        print("    %s" % p, flush=True)
-    subprocess.run(install_command, cwd="vcpkg", shell=True)
-    print("Removing temporary vcpkg files", flush=True)
-    shutil.rmtree("vcpkg/downloads")
-    print("Writing version stamp", flush=True)
     version_file = open("vcpkg/build-version", "w")
     version_file.write(version)
     version_file.close()
--- tools/ci/vcpkg_version.py
+++ tools/ci/vcpkg_version.py
@@ -2,5 +2,5 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-version = "41"
-vcpkg_githash = "61f610845fb206298a69f708104a51d651872877"
+version = "42"
+vcpkg_githash = "0c20b2a97c390e106150837042d921b0939e7ecb
