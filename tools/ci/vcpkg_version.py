7fc314c34fa82aeba7f83be03e9044d29a446ae7
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-11-19 13:58:00 +0100

    Update vcpkg_githash in vcpkg_version.py

--- tools/ci/vcpkg_version.py
+++ tools/ci/vcpkg_version.py
@@ -3,4 +3,4 @@
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
 version = "42"
-vcpkg_githash = "0c20b2a97c390e106150837042d921b0939e7ecb
+vcpkg_githash = "0c20b2a97c390e106150837042d921b0939e7ecb"
