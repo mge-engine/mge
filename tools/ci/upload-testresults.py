a889ee84e810a7ef6dc11f2f5f9592c22332f321
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-11-25 21:20:35 +0100

    File upload

--- tools/ci/upload-testresults.py
+++ tools/ci/upload-testresults.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import requests
 
 env = os.environ.copy()
 
@@ -9,5 +10,17 @@ if env["APPVEYOR_JOB_ID"] is None:
     sys.exit(1)
 
 # Get all files named "test*.xml"
-test_files = glob.glob("test*.xml", root_dir="build")
+test_files = glob.glob("build/test*.xml")
 print("Test files:", test_files)
+
+# URL to upload the test files
+upload_url = "https://ci.appveyor.com/api/testresults/junit/" + env["APPVEYOR_JOB_ID"]
+
+
+for file in test_files:
+    with open(file, "rb") as f:
+        response = requests.post(upload_url, files={"file": f})
+        if response.status_code == 200:
+            print(f"Uploaded {file} successfully")
+        else:
+            print(f"Failed to upload {file}. Status code: {response.status_code}")
