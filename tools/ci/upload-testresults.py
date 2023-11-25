63c30c05095e0ddcef552eab9167b91987adc1d8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-11-25 17:47:58 +0100

    get files from right directory

--- tools/ci/upload-testresults.py
+++ tools/ci/upload-testresults.py
@@ -2,9 +2,6 @@ import os
 import sys
 import glob
 
-current_directory = os.getcwd()
-print("Current directory:", current_directory)
-
 env = os.environ.copy()
 
 if env["APPVEYOR_JOB_ID"] is None:
@@ -12,5 +9,5 @@ if env["APPVEYOR_JOB_ID"] is None:
     sys.exit(1)
 
 # Get all files named "test*.xml"
-test_files = glob.glob("test*.xml")
+test_files = glob.glob("test*.xml", root_dir="build")
 print("Test files:", test_files)
