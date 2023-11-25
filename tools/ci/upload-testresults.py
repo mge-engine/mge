4201e2e20e97157dcc5e6ef5c50e2d33fe9b49e3
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-11-25 16:02:03 +0100

    Test files upload

--- tools/ci/upload-testresults.py
+++ tools/ci/upload-testresults.py
@@ -1,12 +1,16 @@
 import os
 import sys
+import glob
 
 current_directory = os.getcwd()
 print("Current directory:", current_directory)
 
 env = os.environ.copy()
 
-if env["APPVEYOUR_JOB_ID"] is None:
+if env["APPVEYOR_JOB_ID"] is None:
     print("Not running in AppVeyor, exiting", flush=True)
     sys.exit(1)
 
+# Get all files named "test*.xml"
+test_files = glob.glob("test*.xml")
+print("Test files:", test_files)
