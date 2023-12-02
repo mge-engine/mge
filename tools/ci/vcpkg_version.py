49003bae488c6a3be73c7304f6f771199d9fb20e
Alexander Schroeder <schroeder.alexander@googlemail.com>
2023-12-02 16:37:07 +0100

    vcpkg version tag

--- tools/ci/vcpkg_version.py
+++ tools/ci/vcpkg_version.py
@@ -2,5 +2,5 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-version = "43"
+version = "44"
 vcpkg_githash = "16ee2ecb31788c336ace8bb14c21801efb6836e4"
