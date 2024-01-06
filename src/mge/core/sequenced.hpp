54692df7a9a0d1bcec0694097061efb812b2f96d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-01-06 21:20:34 +0100

    Add sequenced class and test cases

---
+++ src/mge/core/sequenced.hpp
@@ -0,0 +1,98 @@
+#pragma once
+#include <atomic>
+#include <functional>
+
+namespace mge {
+
+    /**
+     * @brief A sequenced object having a sequence number.
+     *
+     * @tparam T The type that is sequenced here, different types have different
+     * numbers.
+     */
+    template <typename T> class sequenced
+    {
+    public:
+        using sequence_type = uint64_t;
+        sequenced()
+            : m_sequence(++sequence_instance())
+        {}
+
+        sequenced(const sequenced&) noexcept = default;
+        sequenced& operator=(const sequenced&) noexcept = default;
+
+        sequenced(sequenced&& other) noexcept
+            : m_sequence(other.m_sequence)
+        {
+            other.m_sequence = 0;
+        }
+
+        sequenced& operator=(sequenced&& other) noexcept
+        {
+            m_sequence = other.m_sequence;
+            other.m_sequence = 0;
+            return *this;
+        }
+
+        sequence_type sequence() const noexcept { return m_sequence; }
+
+        bool operator<(const sequenced& other) const noexcept
+        {
+            return m_sequence < other.m_sequence;
+        }
+
+        bool operator<=(const sequenced& other) const noexcept
+        {
+            return m_sequence <= other.m_sequence;
+        }
+
+        bool operator>(const sequenced& other) const noexcept
+        {
+            return m_sequence > other.m_sequence;
+        }
+
+        bool operator>=(const sequenced& other) const noexcept
+        {
+            return m_sequence >= other.m_sequence;
+        }
+
+        bool operator==(const sequenced& other) const noexcept
+        {
+            return m_sequence == other.m_sequence;
+        }
+
+        bool operator!=(const sequenced& other) const noexcept
+        {
+            return m_sequence != other.m_sequence;
+        }
+
+        ~sequenced() = default;
+
+        constexpr explicit operator bool() const noexcept
+        {
+            return m_sequence != 0;
+        }
+
+        constexpr bool has_value() const noexcept { return m_sequence != 0; }
+
+    private:
+        sequence_type m_sequence;
+
+        static std::atomic<sequence_type>& sequence_instance()
+        {
+            static std::atomic<sequence_type> s;
+            return s;
+        }
+    };
+} // namespace mge
+namespace std {
+    template <typename T> struct std::hash<mge::sequenced<T>>
+    {
+        std::size_t operator()(const mge::sequenced<T>& s) const noexcept
+        {
+            return std::hash<typename mge::sequenced<T>::sequence_type>()(
+                s.sequence());
+        }
+    };
+
+} // namespace std
---
+++ src/mge/core/test/test_sequenced.cpp
@@ -0,0 +1,45 @@
+#include "mge/core/sequenced.hpp"
+#include "test/googletest.hpp"
+
+namespace mge {
+
+    TEST(sequenced, sequenced)
+    {
+        sequenced<int> a;
+        sequenced<int> b;
+        EXPECT_LT(a, b);
+        EXPECT_LE(a, b);
+        EXPECT_GT(b, a);
+        EXPECT_GE(b, a);
+        EXPECT_NE(a, b);
+    }
+
+    TEST(sequenced, sequenced_move)
+    {
+        sequenced<int> a;
+        sequenced<int> b(std::move(a));
+        EXPECT_EQ(0, a.sequence());
+        EXPECT_FALSE(a.has_value());
+        EXPECT_NE(0, b.sequence());
+    }
+
+    TEST(sequenced, sequenced_move_assign)
+    {
+        sequenced<int> a;
+        sequenced<int> b;
+        b = std::move(a);
+        EXPECT_EQ(0, a.sequence());
+        EXPECT_FALSE(a.has_value());
+        EXPECT_NE(0, b.sequence());
+    }
+
+    TEST(sequenced, sequenced_copy_assign)
+    {
+        sequenced<int>                a;
+        sequenced<int>                b;
+        sequenced<int>::sequence_type b_seq = b.sequence();
+        b = a;
+        EXPECT_EQ(a.sequence(), b.sequence());
+        EXPECT_NE(b_seq, b.sequence());
+    }
+} // namespace mge
