b5499430a4aae11dca02618d796ce71a7cdc7eca
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-03-16 18:42:34 +0100

    Texture scaffolding

--- src/mge/graphics/texture.cpp
+++ src/mge/graphics/texture.cpp
@@ -4,5 +4,12 @@
 #include "texture.hpp"
 
 namespace mge {
-    texture::~texture() {}
+
+    texture::texture(render_context& ctx, texture_type type)
+        : context_object(ctx)
+        , m_type(type)
+    {}
+
+    texture_type texture::type() const { return m_type; }
+
 } // namespace mge
--- src/mge/graphics/texture.hpp
+++ src/mge/graphics/texture.hpp
@@ -3,16 +3,22 @@
 // All rights reserved.
 #pragma once
 #include "mge/graphics/context_object.hpp"
+#include "mge/graphics/texture_type.hpp"
 
 namespace mge {
 
     class MGEGRAPHICS_EXPORT texture : public context_object
     {
     protected:
-        texture(render_context&);
+        texture(render_context&, texture_type type);
 
     public:
-        virtual ~texture();
+        virtual ~texture() = default;
+
+        texture_type type() const;
+
+    private:
+        texture_type m_type;
     };
 
 } // namespace mge
