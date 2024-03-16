2f220d3978d9ac24f33fda1a810e16c4ea1f1fb1
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-03-16 18:40:02 +0100

    texture type

--- src/mge/graphics/texture_type.hpp
+++ src/mge/graphics/texture_type.hpp
@@ -3,12 +3,45 @@
 // All rights reserved.
 #pragma once
 #include "mge/graphics/dllexport.hpp"
+#include <format>
+#include <iosfwd>
 
 namespace mge {
+    /**
+     * Type of texture.
+     */
     enum class MGEGRAPHICS_EXPORT texture_type
     {
-        TYPE_1D,
-        TYPE_2D,
-        TYPE_3D
+        TYPE_1D, //!< 1D texture
+        TYPE_2D, //!< 2D texture
+        TYPE_3D  //!< 3D texture
     };
-}
+
+    MGEGRAPHICS_EXPORT std::ostream& operator<<(std::ostream&       os,
+                                                const texture_type& t);
+} // namespace mge
+
+template <typename C>
+struct std::formatter<mge::texture_type, C>
+    : std::formatter<std::string_view, C>
+{
+    auto format(const mge::texture_type& t,
+                auto& ctx) const -> std::format_context::iterator
+    {
+        switch (t) {
+        case mge::texture_type::TYPE_1D:
+            return std::formatter<std::string_view, C>::format(
+                std::string_view{"TYPE_1D"},
+                ctx);
+        case mge::texture_type::TYPE_2D:
+            return std::formatter<std::string_view, C>::format(
+                std::string_view{"TYPE_2D"},
+                ctx);
+        case mge::texture_type::TYPE_3D:
+            return std::formatter<std::string_view, C>::format(
+                std::string_view{"TYPE_3D"},
+                ctx);
+        }
+        return std::format_to(ctx.out(), "INVALID({})", static_cast<int>(t));
+    }
+};
