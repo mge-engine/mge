39e01244713c2139b46d00d7115cabc7f59e26c5
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-03-18 22:22:32 +0100

    Remove asset_handle class and its related files

--- src/mge/asset/asset_handle.cpp
+++
@@ -1,7 +0,0 @@
-// mge - Modern Game Engine
-// Copyright (c) 2017-2023 by Alexander Schroeder
-// All rights reserved.
-#include "mge/asset/asset_handle.hpp"
-namespace mge {
-    asset_handle::asset_handle() {}
-} // namespace mge
