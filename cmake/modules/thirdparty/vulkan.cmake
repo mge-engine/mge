cf2f62d92d3ead2606164c87e1382198177a268e
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-04-08 12:18:46 +0200

    Cleanup package usage

--- cmake/modules/thirdparty/vulkan.cmake
+++ cmake/modules/thirdparty/vulkan.cmake
@@ -2,11 +2,30 @@
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
 INCLUDE_GUARD(GLOBAL)
-INCLUDE(FindVulkan)
+FIND_PACKAGE(Vulkan COMPONENTS glslang SPIRV-Tools shaderc_combined REQUIRED)
+
+#COMPONENTS glslang)
 IF(Vulkan_FOUND)
-MESSAGE(STATUS "Vulkan found")
-ADD_LIBRARY(vulkan INTERFACE)
-SET_PROPERTY(TARGET vulkan
-             APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${Vulkan_INCLUDE_DIRS}")
+    MESSAGE(STATUS "Vulkan found")
+    GET_FILENAME_COMPONENT(VULKAN_LIB_DIR ${Vulkan_LIBRARIES} DIRECTORY)
+    # Find debug library
+    FIND_LIBRARY(VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_DEBUG
+    NAMES glslang-default-resource-limitsd
+    PATHS ${VULKAN_LIB_DIR}
+    NO_DEFAULT_PATH)
+    # Find release library
+    FIND_LIBRARY(VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_RELEASE
+    NAMES glslang-default-resource-limits
+    PATHS ${VULKAN_LIB_DIR}
+    NO_DEFAULT_PATH)
+
+    ADD_LIBRARY("Vulkan::glslang-default-resource-limits" STATIC IMPORTED GLOBAL)
+    SET_TARGET_PROPERTIES("Vulkan::glslang-default-resource-limits" PROPERTIES
+                          IMPORTED_LOCATION_DEBUG ${VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_DEBUG}
+                          IMPORTED_LOCATION_RELEASE ${VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_RELEASE})
+
+ELSE()
+    MESSAGE(FATAL_ERROR "Vulkan not found")
 ENDIF()
+
 
