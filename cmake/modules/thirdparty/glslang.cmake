cd8b080854e4a51ad6b7596b44a9a7f7e3a5973c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-04-13 22:15:14 +0200

    Bump Vulkan to 1.3.280.0

--- cmake/modules/thirdparty/glslang.cmake
+++ cmake/modules/thirdparty/glslang.cmake
@@ -1,20 +1,12 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-
-# mge - Modern Game Engine
-# Copyright (c) 2017-2023 by Alexander Schroeder
-# All rights reserved.
-
-#INCLUDE(thirdparty/vulkan)
-#IF(Vulkan_FOUND)
-#    FIND_PACKAGE(glslang CONFIG REQUIRED)
-#    IF(glslang_FOUND)
-#        MESSAGE(STATUS "glslang found")
-#        #ADD_LIBRARY(glslang ALIAS glslang::glslang)
-#        #target_link_libraries(glslang PRIVATE glslang::glslang-default-resource-limits)
-#target_link_libraries(main PRIVATE glslang::OSDependent glslang::glslang glslang::MachineIndependent glslang::GenericCodeGen glslang::glslang-default-resource-limits glslang::OGLCompiler glslang::SPVRemapper glslang::SPIRV glslang::HLSL)
-#    ENDIF()
-#ENDIF()
-
-# glslang::glslang-default-resource-limits
+INCLUDE_GUARD(GLOBAL)
+INCLUDE(thirdparty/vulkan)
+IF(Vulkan_FOUND)
+    FIND_PACKAGE(glslang CONFIG REQUIRED)
+    #target_link_libraries(main PRIVATE glslang::glslang glslang::glslang-default-resource-limits glslang::SPIRV glslang::SPVRemapper)
+    IF(glslang_FOUND)
+        MESSAGE(STATUS "glslang found")
+    ENDIF()
+ENDIF()
--- cmake/modules/thirdparty/vulkan.cmake
+++ cmake/modules/thirdparty/vulkan.cmake
@@ -2,30 +2,9 @@
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
 INCLUDE_GUARD(GLOBAL)
-FIND_PACKAGE(Vulkan COMPONENTS glslang SPIRV-Tools shaderc_combined REQUIRED)
-
-#COMPONENTS glslang)
+FIND_PACKAGE(Vulkan)
 IF(Vulkan_FOUND)
     MESSAGE(STATUS "Vulkan found")
-    GET_FILENAME_COMPONENT(VULKAN_LIB_DIR ${Vulkan_LIBRARIES} DIRECTORY)
-    # Find debug library
-    FIND_LIBRARY(VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_DEBUG
-    NAMES glslang-default-resource-limitsd
-    PATHS ${VULKAN_LIB_DIR}
-    NO_DEFAULT_PATH)
-    # Find release library
-    FIND_LIBRARY(VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_RELEASE
-    NAMES glslang-default-resource-limits
-    PATHS ${VULKAN_LIB_DIR}
-    NO_DEFAULT_PATH)
-
-    ADD_LIBRARY("Vulkan::glslang-default-resource-limits" STATIC IMPORTED GLOBAL)
-    SET_TARGET_PROPERTIES("Vulkan::glslang-default-resource-limits" PROPERTIES
-                          IMPORTED_LOCATION_DEBUG ${VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_DEBUG}
-                          IMPORTED_LOCATION_RELEASE ${VULKAN_GLSLANG_DEFAULT_RESOURCE_LIMITS_RELEASE})
-
-ELSE()
-    MESSAGE(FATAL_ERROR "Vulkan not found")
 ENDIF()
 
 
