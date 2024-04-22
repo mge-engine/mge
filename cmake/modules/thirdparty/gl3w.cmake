2ca9451d8ca0fd497b46a32fe5a4849e29a83bd8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-04-22 18:22:01 +0200

    CMake stuff

--- cmake/modules/thirdparty/gl3w.cmake
+++ cmake/modules/thirdparty/gl3w.cmake
@@ -1,33 +1,30 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-FIND_PACKAGE(OpenGL)
-
+INCLUDE_GUARD(GLOBAL)
+INCLUDE(thirdparty/opengl)
+    
 IF(OpenGL_FOUND)
-    IF(NOT GL3W_FOUND)
-        MESSAGE("-- Did not find gl3w, need to build it from source")
-        FILE(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/external ${CMAKE_BINARY_DIR}/external/gl3w)
-        SET(CMAKE_LIST_CONTENT "
-            CMAKE_MINIMUM_REQUIRED(VERSION 3.27)
-            PROJECT(GL3W)
-            INCLUDE(ExternalProject)
-            EXTERNALPROJECT_ADD(gl3w-external
-                                PREFIX gl3w
-                                GIT_REPOSITORY \"https://github.com/mge-engine/gl3w.git\"
-                                GIT_TAG \"5f8d7fd191ba22ff2b60c1106d7135bb9a335533\"
-                                INSTALL_COMMAND \"\"
-                                BUILD_IN_SOURCE TRUE
-            )
-        ")
-        FILE(WRITE ${CMAKE_BINARY_DIR}/external/gl3w/CMakeLists.txt "${CMAKE_LIST_CONTENT}")
-        EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" . -G${CMAKE_GENERATOR} -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-                         WORKING_DIRECTORY  ${CMAKE_BINARY_DIR}/external/gl3w
+    FILE(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/external ${CMAKE_BINARY_DIR}/external/gl3w)
+    SET(CMAKE_LIST_CONTENT "
+        CMAKE_MINIMUM_REQUIRED(VERSION 3.29.1)
+        PROJECT(GL3W)
+        INCLUDE(ExternalProject)
+        EXTERNALPROJECT_ADD(gl3w-external
+                            PREFIX gl3w
+                            GIT_REPOSITORY \"https://github.com/mge-engine/gl3w.git\"
+                            GIT_TAG \"5f8d7fd191ba22ff2b60c1106d7135bb9a335533\"
+                            INSTALL_COMMAND \"\"
+                            BUILD_IN_SOURCE TRUE
         )
-        EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" --build .
-                         WORKING_DIRECTORY  ${CMAKE_BINARY_DIR}/external/gl3w
-        )
-        SET(GL3W_FOUND "TRUE" CACHE INTERNAL "GL3W_FOUND")
-    ENDIF()
+    ")
+    FILE(WRITE ${CMAKE_BINARY_DIR}/external/gl3w/CMakeLists.txt "${CMAKE_LIST_CONTENT}")
+    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" . -G${CMAKE_GENERATOR} -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+                    WORKING_DIRECTORY  ${CMAKE_BINARY_DIR}/external/gl3w
+    )
+    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" --build .
+                    WORKING_DIRECTORY  ${CMAKE_BINARY_DIR}/external/gl3w
+    )
     ADD_LIBRARY(gl3w STATIC
         ${CMAKE_BINARY_DIR}/external/gl3w/gl3w/src/gl3w-external/src/gl3w.c)
     IF(MSVC)
--- cmake/modules/thirdparty/opengl.cmake
+++ cmake/modules/thirdparty/opengl.cmake
@@ -1,6 +1,7 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
+INCLUDE_GUARD(GLOBAL)
 FIND_PACKAGE(OpenGL)
 IF(OpenGL_FOUND)
     MESSAGE("-- OpenGL found")
