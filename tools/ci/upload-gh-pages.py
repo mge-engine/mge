ae7314df35d9331d53692473b55e3dc49653026e
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-04-25 00:27:02 +0200

    wip: upload documentation

--- tools/ci/upload-gh-pages.py
+++ tools/ci/upload-gh-pages.py
@@ -85,8 +85,8 @@ def upload(branch):
     print("Adding files to git", flush=True)
     run_command(
         ["git", "add", branch + "/manual-html"], cwd="gh-pages")
-#    print("Commit git changes", flush=True)
-#    run_command(["git", "commit", "-m", message], cwd="gh-pages")
+    print("Commit git changes", flush=True)
+    run_command(["git", "commit", "-m", message], cwd="gh-pages")
 
 try:
     if upload_enabled():
