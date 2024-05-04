ed92bde279f2d10cb5f136475c4e6bb861378187
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-05-04 18:23:47 +0200

    adapt env

--- tools/build/setenv-macos.sh
+++ tools/build/setenv-macos.sh
@@ -5,4 +5,6 @@ fi
 export VCPKG_ROOT=`pwd`/../vcpkg
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+export LDFLAGS="$LDFLAGS -L/opt/homebrew/opt/llvm/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
 export MGE_BUILDENV_SET=1
