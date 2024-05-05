1421d688b443c43fb308b6f970408c916d01c1df
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-05-05 09:23:40 +0200

    Remove superfluous #pragma once

--- src/mge/core/test/test_lexical_cast.cpp
+++ src/mge/core/test/test_lexical_cast.cpp
@@ -1,7 +1,6 @@
 // mge - Modern Game Engine
 // Copyright (c) 2017-2023 by Alexander Schroeder
 // All rights reserved.
-#pragma once
 #include "mge/core/lexical_cast.hpp"
 #include "test/googletest.hpp"
 
