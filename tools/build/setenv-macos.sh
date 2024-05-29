57e6bf1890f4fe925b4e6bc54e29a41d6a23a455
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-05-28 20:49:19 -0400

    Add compiler enviromnent to setup

--- tools/build/setenv-macos.sh
+++ tools/build/setenv-macos.sh
@@ -7,4 +7,6 @@ export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
 export LDFLAGS="$LDFLAGS -L/opt/homebrew/opt/llvm/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+export CC=clang
+export CXX=clang++
 export MGE_BUILDENV_SET=1
