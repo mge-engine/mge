e1ece50ef8df52c2e5df55869f199680993ab60c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-07-06 11:35:35 +0200

    Fix #187 (#190)
    Fix #187

---
+++ src/mge/application/test/test_simple_application.cpp
@@ -0,0 +1,38 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/application/application.hpp"
+#include "mge/core/trace.hpp"
+
+MGE_DEFINE_TRACE(SIMPLEAPP);
+
+namespace mge {
+    class simpleapp : public application
+    {
+    public:
+        simpleapp() = default;
+
+        void setup() override
+        {
+            MGE_DEBUG_TRACE(SIMPLEAPP) << "Setup simpleapp";
+        }
+
+        void teardown() override
+        {
+            MGE_DEBUG_TRACE(SIMPLEAPP) << "Teardown simpleapp";
+        }
+
+        void run() override
+        {
+            MGE_DEBUG_TRACE(SIMPLEAPP) << "Run simpleapp";
+            set_quit();
+            application::run();
+        }
+    };
+
+    MGE_REGISTER_IMPLEMENTATION(simpleapp,
+                                mge::application,
+                                test_simple_application);
+} // namespace mge
+
+MGE_MAINFUNCTION
