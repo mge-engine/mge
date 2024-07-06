50fa495348bbf6dcd5076b83941523b21b469de9
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-07-06 20:54:22 +0200

    Resurrect Vulkan

--- src/modules/vulkan/render_system.cpp
+++ src/modules/vulkan/render_system.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
 #include "render_system.hpp"
 #include "mge/core/parameter.hpp"
 #include "mge/core/trace.hpp"
