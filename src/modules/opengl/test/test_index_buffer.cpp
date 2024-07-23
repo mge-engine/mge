ec8237c9611c3f3118d7935319473f6481fab852
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-07-23 21:20:24 +0200

    More benchmark tests

--- src/modules/opengl/test/test_index_buffer.cpp
+++ src/modules/opengl/test/test_index_buffer.cpp
@@ -4,7 +4,10 @@
 #include "mge/graphics/data_type.hpp"
 #include "mge/graphics/index_buffer.hpp"
 #include "mge/graphics/render_context.hpp"
+
 #include "opengl_test.hpp"
+
+#include "test/benchmark.hpp"
 
 class index_buffer_test : public mge::opengl::opengltest
 {};
@@ -31,4 +34,20 @@ TEST_F(index_buffer_test, map_unmap)
         data[i] = i;
     }
     buffer->unmap();
+}
+
+TEST_F(index_buffer_test, bench_index_buffer_create_map_unmap)
+{
+    auto& context = m_window->render_context();
+    int*  data = new int[100];
+    for (int i = 0; i < 100; ++i) {
+        data[i] = i;
+    }
+    mge::benchmark().show_results().run("index_buffer_creation", [&]() {
+        auto buffer =
+            context.create_index_buffer(mge::data_type::INT32, 400, nullptr);
+        int* buffer_data = static_cast<int*>(buffer->map());
+        memcpy(buffer_data, data, 100 * sizeof(int));
+        buffer->unmap();
+    });
 }
