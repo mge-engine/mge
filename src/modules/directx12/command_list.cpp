f73c723b68a169409be1019ae567ecc096d5ebab
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-07-27 14:36:11 +0200

    frame less command list

--- src/modules/directx12/command_list.cpp
+++ src/modules/directx12/command_list.cpp
@@ -3,6 +3,7 @@
 // All rights reserved.
 #include "command_list.hpp"
 #include "error.hpp"
+#include "frame_command_list.hpp"
 #include "index_buffer.hpp"
 #include "program.hpp"
 #include "render_context.hpp"
@@ -10,61 +11,13 @@
 #include "vertex_buffer.hpp"
 
 namespace mge::dx12 {
-#include <d3d12.h> // Include the necessary header file
-
     command_list::command_list(render_context& context)
-        : mge::command_list(context, true)
+        : mge::command_list(context, false)
         , m_dx12_context(context)
         , m_color_set(false)
-    {
-        m_rasterizer_desc = {
-            .FillMode = D3D12_FILL_MODE_SOLID,
-            .CullMode = D3D12_CULL_MODE_BACK,
-            .FrontCounterClockwise = FALSE,
-            .DepthBias = D3D12_DEFAULT_DEPTH_BIAS,
-            .DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP,
-            .SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS,
-            .DepthClipEnable = TRUE,
-            .MultisampleEnable = FALSE,
-            .AntialiasedLineEnable = FALSE,
-            .ForcedSampleCount = 0,
-            .ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF};
+    {}
 
-        m_blend_desc = {.AlphaToCoverageEnable = FALSE,
-                        .IndependentBlendEnable = FALSE};
-
-        m_blend_desc.RenderTarget[0] = {.BlendEnable = FALSE,
-                                        .LogicOpEnable = FALSE,
-                                        .SrcBlend = D3D12_BLEND_ONE,
-                                        .DestBlend = D3D12_BLEND_ZERO,
-                                        .BlendOp = D3D12_BLEND_OP_ADD,
-                                        .SrcBlendAlpha = D3D12_BLEND_ONE,
-                                        .DestBlendAlpha = D3D12_BLEND_ZERO,
-                                        .BlendOpAlpha = D3D12_BLEND_OP_ADD,
-                                        .LogicOp = D3D12_LOGIC_OP_NOOP,
-                                        .RenderTargetWriteMask =
-                                            D3D12_COLOR_WRITE_ENABLE_ALL};
-
-        HRESULT rc = m_dx12_context.device()->CreateCommandAllocator(
-            D3D12_COMMAND_LIST_TYPE_BUNDLE,
-            IID_PPV_ARGS(&m_bundle_allocator));
-        CHECK_HRESULT(rc, ID3D12Device, CreateCommandAllocator);
-    }
-
-    command_list::~command_list() { release_draw_list(); }
-
-    void command_list::release_draw_list()
-    {
-        for (auto& d : m_draw_list) {
-            if (std::get<1>(d)) {
-                std::get<1>(d)->Release();
-            }
-            if (std::get<2>(d)) {
-                std::get<2>(d)->Release();
-            }
-        }
-        m_draw_list.clear();
-    }
+    command_list::~command_list() {}
 
     void command_list::clear(const rgba_color& c)
     {
@@ -74,88 +27,25 @@ namespace mge::dx12 {
 
     void command_list::draw(const mge::draw_command& command)
     {
-        ID3D12GraphicsCommandList* command_list = nullptr;
-
-        auto rc = m_dx12_context.device()->CreateCommandList(
-            0,
-            D3D12_COMMAND_LIST_TYPE_BUNDLE,
-            m_bundle_allocator.Get(),
-            nullptr,
-            IID_PPV_ARGS(&command_list));
-        CHECK_HRESULT(rc, ID3D12Device, CreateCommandList);
-
-        auto vs_ref = dx12_program(*command.program())
-                          .program_shader(shader_type::VERTEX);
-        auto ps_ref = dx12_program(*command.program())
-                          .program_shader(shader_type::FRAGMENT);
-        auto& vs = dx12_shader(*vs_ref);
-        auto& ps = dx12_shader(*ps_ref);
-
-        D3D12_GRAPHICS_PIPELINE_STATE_DESC pso_desc = {};
-        pso_desc.InputLayout = {vs.input_layout(), vs.input_layout_count()};
-        pso_desc.pRootSignature =
-            dx12_program(*command.program()).root_signature();
-        pso_desc.VS = {vs.code()->GetBufferPointer(),
-                       vs.code()->GetBufferSize()};
-        pso_desc.PS = {ps.code()->GetBufferPointer(),
-                       ps.code()->GetBufferSize()};
-        pso_desc.RasterizerState = m_rasterizer_desc;
-        pso_desc.BlendState = m_blend_desc;
-        pso_desc.DepthStencilState = {.DepthEnable = false,
-                                      .StencilEnable = false};
-        pso_desc.SampleMask = UINT_MAX;
-        pso_desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-        pso_desc.NumRenderTargets = 1;
-        pso_desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-        pso_desc.SampleDesc = {.Count = 1, .Quality = 0};
-
-        auto root_signature = dx12_program(*command.program()).root_signature();
-        ID3D12PipelineState* pipeline_state = nullptr;
-        try {
-            rc = m_dx12_context.device()->CreateGraphicsPipelineState(
-                &pso_desc,
-                IID_PPV_ARGS(&pipeline_state));
-
-            CHECK_HRESULT(rc, ID3D12Device, CreateGraphicsPipelineState);
-
-            command_list->SetGraphicsRootSignature(root_signature);
-            command_list->SetPipelineState(pipeline_state);
-            command_list->IASetPrimitiveTopology(
-                D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-            command_list->IASetVertexBuffers(
-                0,
-                1,
-                &dx12_vertex_buffer(*command.vertices()).view());
-            command_list->IASetIndexBuffer(
-                &dx12_index_buffer(*command.indices()).view());
-            command_list->DrawIndexedInstanced(
-                static_cast<UINT>(command.indices()->element_count()),
-                1,
-                0,
-                0,
-                0);
-
-            command_list->Close();
-            m_draw_list.push_back(
-                std::make_tuple(root_signature, pipeline_state, command_list));
-        } catch (...) {
-            if (pipeline_state) {
-                pipeline_state->Release();
-            }
-            command_list->Release();
-            throw;
-        }
+        m_draw_commands.push_back(command);
     }
 
     void command_list::execute()
-    { // m_dx12_context.execute(*this);
-    }
-
-    bool command_list::empty() const { return m_draw_list.empty(); }
-
-    const command_list::draw_list& command_list::elements() const
     {
-        return m_draw_list;
+        if (m_draw_commands.empty() && !m_color_set) {
+            return;
+        }
+        auto frame_cmds = m_dx12_context.create_current_frame_command_list();
+        if (m_color_set) {
+            frame_cmds->clear(m_clear_color);
+        }
+        for (const auto& command : m_draw_commands) {
+            frame_cmds->draw(command);
+        }
+        frame_cmds->finish();
+        frame_cmds->execute();
     }
+
+    void command_list::finish() { return; }
 
 } // namespace mge::dx12
--- src/modules/directx12/command_list.hpp
+++ src/modules/directx12/command_list.hpp
@@ -14,45 +14,18 @@ namespace mge::dx12 {
     class command_list : public mge::command_list
     {
     public:
-        using draw_data = std::tuple<ID3D12RootSignature*,
-                                     ID3D12PipelineState*,
-                                     ID3D12GraphicsCommandList*>;
-        using draw_list = small_vector<draw_data, 4>;
-
         command_list(render_context& context);
         ~command_list();
 
         void clear(const rgba_color& c) override;
         void draw(const mge::draw_command& command) override;
+        void finish() override;
         void execute() override;
 
-        inline bool              color_set() const { return m_color_set; }
-        inline const rgba_color& clear_color() const { return m_clear_color; }
-
-        bool             empty() const;
-        const draw_list& elements() const;
-
     private:
-        void release_draw_list();
-
-        render_context&                 m_dx12_context;
-        com_ptr<ID3D12CommandAllocator> m_bundle_allocator;
-
-        draw_list             m_draw_list;
-        rgba_color            m_clear_color;
-        D3D12_RASTERIZER_DESC m_rasterizer_desc;
-        D3D12_BLEND_DESC      m_blend_desc;
-        bool                  m_color_set;
+        render_context&                          m_dx12_context;
+        mge::small_vector<mge::draw_command, 16> m_draw_commands;
+        rgba_color                               m_clear_color;
+        bool                                     m_color_set;
     };
-
-    inline command_list& dx12_command_list(mge::command_list& cl)
-    {
-        return static_cast<command_list&>(cl);
-    }
-
-    inline const command_list& dx12_command_list(const mge::command_list& cl)
-    {
-        return static_cast<const command_list&>(cl);
-    }
-
 } // namespace mge::dx12
