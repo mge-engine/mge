bb49f84023db67c5011f0b3df88731c534c3ecaf
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-08-18 17:17:09 +0200

    More scaffolding

---
+++ src/mge/script/script_context.cpp
@@ -0,0 +1,10 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/script/script_context.hpp"
+#include "mge/core/trace.hpp"
+// #include "mge/script/script_binder.hpp"
+
+namespace mge {
+    script_context::script_context() {}
+} // namespace mge
---
+++ src/mge/script/script_context.hpp
@@ -0,0 +1,34 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/script/dllexport.hpp"
+#include "mge/script/script_fwd.hpp"
+
+namespace mge {
+
+    class MGE_SCRIPT_EXPORT script_context
+    {
+    protected:
+        script_context();
+
+    public:
+        virtual ~script_context() = default;
+
+        /**
+         * Evaluate a script.
+         * @param script script to evaluate
+         */
+        virtual void eval(const std::string& script) = 0;
+
+        /**
+         * Invoke interpreter as if called from command line.
+         * @param argc argument count
+         * @param argv argument values
+         * @return return code
+         */
+        virtual int main(int argc, const char** argv) = 0;
+    };
+
+    MGE_DECLARE_REF(script_context);
+
+} // namespace mge
