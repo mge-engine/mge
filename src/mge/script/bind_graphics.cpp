137125fc0e548a588ed76016aaac1a09d9aa74b7
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-08-30 19:16:46 +0200

    scaffolding

---
+++ src/mge/script/bind_graphics.cpp
@@ -0,0 +1,19 @@
+#include "mge/script/function.hpp"
+#include "mge/script/module.hpp"
+#include "mge/script/script_binder.hpp"
+#include "mge/script/type.hpp"
+
+namespace mge::script {
+    class graphics_script_binder : public script_binder
+    {
+    public:
+        graphics_script_binder() = default;
+
+        void bind() override { mge::script::module mge("mge"); }
+    };
+
+    MGE_REGISTER_IMPLEMENTATION(graphics_script_binder,
+                                mge::script_binder,
+                                mgegraphics graphics);
+
+} // namespace mge::script
