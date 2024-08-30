c77f4680b24d0b8bd1097c44fa7f9a3c72fa31e5
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-08-30 20:48:33 +0200

    rm obsolete

--- src/mge/script.old/call_context.cpp
+++
@@ -1,8 +0,0 @@
-// mge - Modern Game Engine
-// Copyright (c) 2017-2023 by Alexander Schroeder
-// All rights reserved.
-#include "mge/script/call_context.hpp"
-
-namespace mge::script {
-    call_context::~call_context() {}
-}
--- src/mge/script.old/invoke_context.cpp
+++
@@ -1,6 +0,0 @@
-// mge - Modern Game Engine
-// Copyright (c) 2017-2023 by Alexander Schroeder
-// All rights reserved.
-#include "mge/script/invoke_context.hpp"
-
-namespace mge::script {}
