f6b2b858bf35530200ed54393412e7f5f3d5e54d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-08-30 19:17:14 +0200

    fix copyright

--- src/mge/script/bind_graphics.cpp
+++ src/mge/script/bind_graphics.cpp
@@ -1,3 +1,6 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
 #include "mge/script/function.hpp"
 #include "mge/script/module.hpp"
 #include "mge/script/script_binder.hpp"
