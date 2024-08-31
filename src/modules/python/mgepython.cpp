2edf9b5b2157b5e63265c56acdd5ac30c23490ae
Alexander Schroeder <schroeder.alexander@googlemail.com>
2024-08-31 10:27:24 +0200

    python: scaffolding

--- src/modules/python/mgepython.cpp
+++ src/modules/python/mgepython.cpp
@@ -4,12 +4,9 @@
 #include "mge/core/configuration.hpp"
 #include "mge/core/module.hpp"
 #include "mge/core/trace.hpp"
-#include "mge/script/module.hpp"
 #include "mge/script/script_context.hpp"
 #include "mge/script/script_engine.hpp"
-#include "mge/script/script_fwd.hpp"
 
-#include <iostream>
 
 MGE_DEFINE_TRACE(MGEPYTHON);
 
@@ -24,23 +21,20 @@ int main(int argc, const char** argv)
         mge::script_engine_ref engine = mge::script_engine::create("python");
         MGE_DEBUG_TRACE(MGEPYTHON) << "Creating python context";
         mge::script_context_ref context = engine->create_context();
-        MGE_DEBUG_TRACE(MGEPYTHON) << "Binding global module";
-        mge::script::module global_module;
-        context->bind(global_module);
-        MGE_DEBUG_TRACE(MGEPYTHON) << "Run main";
+        MGE_DEBUG_TRACE(MGEPYTHON) << "Binding types";
+        //
         int rc = context->main(argc, argv);
         MGE_DEBUG_TRACE(MGEPYTHON) << "Python main returned: " << rc;
         MGE_DEBUG_TRACE(MGEPYTHON) << "Releasing context";
         context.reset();
         MGE_DEBUG_TRACE(MGEPYTHON) << "Context released";
         return rc;
-    } catch (const mge::exception& mge_ex) {
-        MGE_ERROR_TRACE(MGEPYTHON)
-            << "Caught mge exception: " << mge_ex.details();
-        std::cerr << mge_ex.details();
-    } catch (const std::exception& std_ex) {
-        MGE_ERROR_TRACE(MGEPYTHON) << "Caught exception: " << std_ex.what();
-        std::cerr << "Caught exception: " << std_ex.what();
+    } catch (const mge::exception& e) {
+        MGE_ERROR_TRACE(MGEPYTHON) << "Caught exception: " << e.details();
+        std::cerr << e.details();
+    } catch (const std::exception& e) {
+        MGE_ERROR_TRACE(MGEPYTHON) << "Caught exception: " << e.what();
+        std::cerr << "Caught exception: " << e.what();
     }
     return 1;
 }
