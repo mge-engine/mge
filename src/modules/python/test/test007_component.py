3c0b2cb92aa5cd579a8709b4d10edc5818c799b2
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-03-08 20:24:47 +0100

    Conditionally include render system test and disable component registry test

--- src/modules/python/test/test007_component.py
+++ src/modules/python/test/test007_component.py
@@ -8,5 +8,6 @@ import __mge__
 class TestComponent(unittest.TestCase):
 
     def test_registry(self):
-        r = __mge__.__component__registry__()
-        self.assertIsNotNone(r)
+        #r = __mge__.__component__registry__()
+        #self.assertIsNotNone(r)
+        pass
