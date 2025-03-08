a96767254015619a531a4b41b9313b7f2b53c1c8
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-03-08 20:24:34 +0100

    Refactor Python component registry object naming

--- src/modules/python/python_component_registry.cpp
+++ src/modules/python/python_component_registry.cpp
@@ -15,12 +15,12 @@ namespace mge {
 
 namespace mge::python {
 
-    struct python_component_registry_object
+    struct python_component_object
     {
         PyObject_HEAD static python_context* context;
     };
 
-    python_context* python_component_registry_object::context = nullptr;
+    python_context* python_component_object::context = nullptr;
 
     PyObject* component::register_component(PyObject* self, PyObject* args)
     {
@@ -57,53 +57,53 @@ namespace mge::python {
         // clang-format off
         PyVarObject_HEAD_INIT(nullptr, 0)
         // clang-format on
-        "__mge__.__component_registry__",         /* tp_name */
-        sizeof(python_component_registry_object), /* tp_basicsize */
-        0,                                        /* tp_itemsize */
-        0,                                        /* tp_dealloc */
-        0,                                        /* tp_print */
-        0,                                        /* tp_getattr */
-        0,                                        /* tp_setattr */
-        0,                                        /* tp_reserved */
-        0,                                        /* tp_repr */
-        0,                                        /* tp_as_number */
-        0,                                        /* tp_as_sequence */
-        0,                                        /* tp_as_mapping */
-        0,                                        /* tp_hash */
-        0,                                        /* tp_call */
-        0,                                        /* tp_str */
-        0,                                        /* tp_getattro */
-        0,                                        /* tp_setattro */
-        0,                                        /* tp_as_buffer */
-        Py_TPFLAGS_DEFAULT,                       /* tp_flags */
-        "Wrapper for component registration",     /* tp_doc */
-        0,                                        /* tp_traverse */
-        0,                                        /* tp_clear */
-        0,                                        /* tp_richcompare */
-        0,                                        /* tp_weaklistoffset */
-        0,                                        /* tp_iter */
-        0,                                        /* tp_iternext */
-        python_component_methods,                 /* tp_methods */
-        0,                                        /* tp_members */
-        0,                                        /* tp_getset */
-        0,                                        /* tp_base */
-        0,                                        /* tp_dict */
-        0,                                        /* tp_descr_get */
-        0,                                        /* tp_descr_set */
-        0,                                        /* tp_dictoffset */
-        0,                                        /* tp_init */
-        0,                                        /* tp_alloc */
-        0,                                        /* tp_new */
-        0,                                        /* tp_free */
-        0,                                        /* tp_is_gc */
-        0,                                        /* tp_bases */
-        0,                                        /* tp_mro */
-        0,                                        /* tp_cache */
-        0,                                        /* tp_subclasses */
-        0,                                        /* tp_weaklist */
-        0,                                        /* tp_del */
-        0,                                        /* tp_version_tag */
-        0,                                        /* tp_finalize */
+        "__mge__.__component__registry__",    /* tp_name */
+        sizeof(python_component_object),      /* tp_basicsize */
+        0,                                    /* tp_itemsize */
+        0,                                    /* tp_dealloc */
+        0,                                    /* tp_print */
+        0,                                    /* tp_getattr */
+        0,                                    /* tp_setattr */
+        0,                                    /* tp_reserved */
+        0,                                    /* tp_repr */
+        0,                                    /* tp_as_number */
+        0,                                    /* tp_as_sequence */
+        0,                                    /* tp_as_mapping */
+        0,                                    /* tp_hash */
+        0,                                    /* tp_call */
+        0,                                    /* tp_str */
+        0,                                    /* tp_getattro */
+        0,                                    /* tp_setattro */
+        0,                                    /* tp_as_buffer */
+        Py_TPFLAGS_DEFAULT,                   /* tp_flags */
+        "Wrapper for component registration", /* tp_doc */
+        0,                                    /* tp_traverse */
+        0,                                    /* tp_clear */
+        0,                                    /* tp_richcompare */
+        0,                                    /* tp_weaklistoffset */
+        0,                                    /* tp_iter */
+        0,                                    /* tp_iternext */
+        python_component_methods,             /* tp_methods */
+        0,                                    /* tp_members */
+        0,                                    /* tp_getset */
+        0,                                    /* tp_base */
+        0,                                    /* tp_dict */
+        0,                                    /* tp_descr_get */
+        0,                                    /* tp_descr_set */
+        0,                                    /* tp_dictoffset */
+        0,                                    /* tp_init */
+        0,                                    /* tp_alloc */
+        0,                                    /* tp_new */
+        0,                                    /* tp_free */
+        0,                                    /* tp_is_gc */
+        0,                                    /* tp_bases */
+        0,                                    /* tp_mro */
+        0,                                    /* tp_cache */
+        0,                                    /* tp_subclasses */
+        0,                                    /* tp_weaklist */
+        0,                                    /* tp_del */
+        0,                                    /* tp_version_tag */
+        0,                                    /* tp_finalize */
     };
 
     void component::create_registry_type(const python_module_ref& module,
@@ -121,6 +121,6 @@ namespace mge::python {
                            reinterpret_cast<PyObject*>(&component_type));
         error::check_error();
 
-        python_component_registry_object::context = context;
+        python_component_object::context = context;
     }
 } // namespace mge::python
