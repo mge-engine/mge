d2ab9c0768582c598fa4cc45358ba202381f4983
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-03-09 11:12:50 +0100

    Temporarily disable render system test due to application instance requirement

--- src/modules/python/test/test006_render_system.py
+++ src/modules/python/test/test006_render_system.py
@@ -6,6 +6,7 @@ import mge
 
 class TestRenderSystem(unittest.TestCase):
 
+    @unittest.skip("Temporarily disabled, need application instance")
     def test_create(self):
         s = mge.render_system.create()
         self.assertIsNotNone(s)
