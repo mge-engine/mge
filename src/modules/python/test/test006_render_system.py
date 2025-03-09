64d6f6272c4161dcaaaea6c75b48039f13be8d9c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-03-09 11:15:51 +0100

    Remove skip decorator from render system test

--- src/modules/python/test/test006_render_system.py
+++ src/modules/python/test/test006_render_system.py
@@ -5,7 +5,6 @@ import unittest
 import mge
 
 class TestRenderSystem(unittest.TestCase):
-
     @unittest.skip("Temporarily disabled, need application instance")
     def test_create(self):
         s = mge.render_system.create()
