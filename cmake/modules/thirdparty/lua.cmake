bd8d364ab09843b4bfba4137614d1a23de6efd17
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-04-11 21:55:43 +0200

    Remove scripting for now

--- cmake/modules/thirdparty/lua.cmake
+++
@@ -1,9 +0,0 @@
-# mge - Modern Game Engine
-# Copyright (c) 2017-2023 by Alexander Schroeder
-# All rights reserved.
-FIND_PACKAGE(Lua 5.4 REQUIRED)
-##SET_PROPERTY(TARGET lua
-##             PROPERTY IMPORTED_LOCATION "${LUA_LIBRARY}")
-SET_PROPERTY(TARGET lua
-             APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${LUA_INCLUDE_DIR}")
-
--- src/mge/script/script_binder.cpp
+++
@@ -1,9 +0,0 @@
-// mge - Modern Game Engine
-// Copyright (c) 2017-2023 by Alexander Schroeder
-// All rights reserved.
-#include "mge/script/script_binder.hpp"
-
-namespace mge {
-
-    MGE_REGISTER_COMPONENT(script_binder);
-}
