f5892fc9493768eff96c0f7edcb5edf03835c84d
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-04-18 22:04:12 +0200

    Allow unregistered options

--- src/mge/asset/mgeassettool.cpp
+++ src/mge/asset/mgeassettool.cpp
@@ -10,29 +10,36 @@ namespace po = boost::program_options;
 int main(int argc, const char** argv)
 {
     try {
-        po::options_description desc("Generic options");
-        desc.add_options()("help,h",
-                           "Show help message")("version,v",
-                                                "Show version information");
-        po::variables_map vm;
-        auto              parsed = po::parse_command_line(argc, argv, desc);
+        po::options_description generic("Generic options");
+        generic.add_options()("help,h",
+                              "Show help message")("version,v",
+                                                   "Show version information");
+
+        std::vector<std::string> args(argv + 1, argv + argc);
+        po::variables_map        vm;
+        po::parsed_options       parsed =
+            po::command_line_parser(args)
+                .options(generic)
+                .allow_unregistered() // Allow subcommand and its args to pass
+                                      // through
+                .run();
         po::store(parsed, vm);
         po::notify(vm);
+
+        std::vector<std::string> remaining =
+            po::collect_unrecognized(parsed.options, po::include_positional);
 
         if (vm.count("version")) {
             std::cout << "mgeassettool version <<unknown>>" << std::endl;
             return 0;
-        } else if (vm.count("help") || argc == 1) {
+        } else if (vm.count("help") || argc == 1 || remaining.empty()) {
             std::cout << "usage: mgeassettool [options] [<command> [command "
                          "options] [command arguments]]"
                       << std::endl
                       << std::endl;
-            std::cout << desc << std::endl;
+            std::cout << generic << std::endl;
             return 0;
         }
-
-        std::cout << "Not yet implemented: mgeassettool" << std::endl;
-
         return 0;
     } catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
