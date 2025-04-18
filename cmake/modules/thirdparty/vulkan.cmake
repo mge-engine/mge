79285029c25ea4c5f9480157023644a5e7c736b7
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-04-18 08:03:25 +0200

    Update Vulkan (#238)

--- cmake/modules/thirdparty/vulkan.cmake
+++ cmake/modules/thirdparty/vulkan.cmake
@@ -1,10 +1,11 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-INCLUDE_GUARD(GLOBAL)
+INCLUDE_GUARD(GLOBAL) 
 FIND_PACKAGE(Vulkan)
 IF(Vulkan_FOUND)
-    MESSAGE(STATUS "Vulkan found")
+    MESSAGE(STATUS "Vulkan found at ${Vulkan_INCLUDE_DIRS}")
+    MESSAGE(STATUS "Vulkan library found at ${Vulkan_LIBRARY}")
     FIND_PACKAGE(VulkanMemoryAllocator CONFIG REQUIRED)
 ENDIF()
 
