c4e0d8c18692f4a0d013244c25ed60a1f39595c2
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-04-19 19:10:44 +0200

    Create software component interface

---
+++ src/mge/core/software_component.cpp
@@ -0,0 +1,8 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/core/software_component.hpp"
+
+namespace mge {
+    MGE_REGISTER_COMPONENT(software_component);
+}
