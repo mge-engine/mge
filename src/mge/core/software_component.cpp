e639cad42b398210a9f8d6f106586db0095a13ab
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-04-20 10:27:07 +0200

    Basic software component finished

--- src/mge/core/software_component.cpp
+++ src/mge/core/software_component.cpp
@@ -2,7 +2,14 @@
 // Copyright (c) 2017-2023 by Alexander Schroeder
 // All rights reserved.
 #include "mge/core/software_component.hpp"
+#include "mge/core/singleton.hpp"
+#include "mge/core/stdexceptions.hpp"
 #include "mge/version.hpp"
+
+#include <set>
+#include <string_view>
+
+using namespace std::literals::string_view_literals;
 
 namespace mge {
     MGE_REGISTER_COMPONENT(software_component);
@@ -13,9 +20,50 @@ namespace mge {
                                                    MGE_PATCHLEVEL),
                                   MGE_BUILD);
 
+    class software_component_list
+    {
+    public:
+        software_component_list() = default;
+        ~software_component_list() = default;
+
+        software_component_ref get(std::string_view name,
+                                   bool throw_no_such_element = false)
+        {
+            auto it = m_components.find(name);
+            if (it != m_components.end()) {
+                return it->second;
+            }
+            software_component::implementations(
+                [&](std::string_view impl_name) {
+                    auto impl_it =
+                        m_software_component_implementations.find(impl_name);
+                    if (impl_it == m_software_component_implementations.end()) {
+                        m_software_component_implementations.insert(
+                            std::string(impl_name.begin(), impl_name.end()));
+                        auto component = software_component::create(impl_name);
+                        m_components.insert(
+                            std::make_pair(component->name(), component));
+                    }
+                });
+            it = m_components.find(name);
+            if (it != m_components.end()) {
+                return it->second;
+            }
+            if (throw_no_such_element) {
+                MGE_THROW(mge::no_such_element)
+                    << "Software component '" << name << "' not found";
+            }
+            return nullptr;
+        }
+
+        std::map<std::string, software_component_ref, std::less<>> m_components;
+        std::set<std::string, std::less<>> m_software_component_implementations;
+    };
+
+    static singleton<software_component_list> s_software_component_list;
+
     software_component_ref software_component::mge()
     {
-        software_component_ref result;
-        return result;
+        return s_software_component_list->get(std::string_view("mge"), true);
     }
 } // namespace mge
