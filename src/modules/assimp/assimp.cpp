aceeb017f92a5b3c47b05724f670f1ec8578c9a6
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-05-07 19:39:47 +0200

    wip: asset load

--- src/modules/assimp/assimp.cpp
+++ src/modules/assimp/assimp.cpp
@@ -6,10 +6,13 @@
 #include "mge/asset/asset_type.hpp"
 #include "mge/core/checked_cast.hpp"
 #include "mge/core/memory.hpp"
+#include "mge/core/stdexceptions.hpp"
 #include "mge/graphics/graphics_fwd.hpp"
 #include "mge/graphics/image.hpp"
 #include "mge/graphics/memory_image.hpp"
 
+#include <assimp/IOStream.hpp>  // C++ stream interface
+#include <assimp/IOSystem.hpp>  // C++ IOSystem interface
 #include <assimp/Importer.hpp>  // C++ importer interface
 #include <assimp/postprocess.h> // Post processing flags
 #include <assimp/scene.h>       // Output data structure
@@ -47,9 +50,109 @@ namespace mge {
         }
     };
 
+    class assimp_iostream : public Assimp::IOStream
+    {
+    public:
+        assimp_iostream(mge::input_stream_ref stream)
+            : m_stream(stream)
+        {}
+
+        size_t Read(void* pvBuffer, size_t pSize, size_t pCount) override
+        {
+            return m_stream->read(pvBuffer, pSize * pCount) / pSize;
+        }
+
+        size_t Write(const void* /*pvBuffer*/,
+                     size_t /*pSize*/,
+                     size_t /*pCount*/) override
+        {
+            return 0;
+        }
+
+        aiReturn Seek(size_t pOffset, aiOrigin pOrigin) override
+        {
+            mge::input_stream::offset_type offset_after_seek = 0;
+            switch (aiOrigin(pOrigin)) {
+            case aiOrigin_SET:
+                offset_after_seek =
+                    m_stream->seek(pOffset, mge::input_stream::POS_BEG);
+                break;
+            case aiOrigin_CUR:
+                offset_after_seek =
+                    m_stream->seek(pOffset, mge::input_stream::POS_CUR);
+                break;
+            case aiOrigin_END:
+                offset_after_seek =
+                    m_stream->seek(pOffset, mge::input_stream::POS_END);
+                break;
+            default:
+                MGE_THROW(mge::illegal_argument) << "Invalid seek origin";
+            }
+
+            return offset_after_seek == -1 ? aiReturn_FAILURE
+                                           : aiReturn_SUCCESS;
+        }
+
+        size_t Tell() const override
+        {
+            return 0;
+        }
+
+        size_t FileSize() const override
+        {
+            return 0;
+        }
+
+        void Flush() override {}
+
+    private:
+        mge::input_stream_ref m_stream;
+    };
+
+    class assimp_iosystem : public Assimp::IOSystem
+    {
+    public:
+        assimp_iosystem(mge::input_stream_ref stream)
+            : m_stream(stream)
+        {
+            m_assimp_stream = std::make_shared<assimp_iostream>(stream);
+        }
+
+        ~assimp_iosystem() = default;
+
+        bool Exists(const char* pFile) const override
+        {
+            return true;
+        }
+
+        char getOsSeparator() const override
+        {
+            return '/';
+        }
+
+        Assimp::IOStream* Open(const char* pFile, const char* pMode) override
+        {
+            return m_assimp_stream.get();
+        }
+
+        void Close(Assimp::IOStream* pFile) override
+        {
+            // No need to close the stream, it is managed by the asset system
+        }
+
+    private:
+        mge::input_stream_ref            m_stream;
+        std::shared_ptr<assimp_iostream> m_assimp_stream;
+    };
+
     std::any assimp_loader::load(const mge::asset& a)
     {
-        return nullptr;
+        Assimp::Importer      importer;
+        mge::input_stream_ref stream = a.data();
+        assimp_iosystem       iosystem(stream);
+
+        importer.SetIOHandler(&iosystem);
+        return std::any();
     }
 
     std::span<mge::asset_type> assimp_loader::handled_types() const
