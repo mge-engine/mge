318c9b9fabcc8ad91d5d94de631b66408af5a9bb
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-05-19 22:11:03 +0200

    scaffolding: scene

---
+++ src/mge/scene/scene.cpp
@@ -0,0 +1,9 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/scene/scene.hpp"
+#include "mge/core/trace.hpp"
+
+namespace mge {
+    MGE_DEFINE_TRACE(SCENE);
+} // namespace mge
---
+++ src/mge/scene/test/test_scene.cpp
@@ -0,0 +1,8 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "test/googletest.hpp"
+
+#include "mge/scene/scene.hpp"
+
+TEST(scene, create) {}
