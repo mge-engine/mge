e827db4118708f518e0293d0feb6aa81f31097a4
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-06-08 21:31:24 +0200

    nuklear options

--- src/mge/ui/nuklear.cpp
+++ src/mge/ui/nuklear.cpp
@@ -1,4 +1,6 @@
 // mge - Modern Game Engine
 // Copyright (c) 2017-2023 by Alexander Schroeder
 // All rights reserved.
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 #include "mge/ui/nuklear.hpp"
