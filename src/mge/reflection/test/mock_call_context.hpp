72db4e4c8c9b902028a50c42a46f0e542d86ed6a
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-11-22 18:59:10 +0100

    Add parameter methods for all trivial types and implement parameterized constructor registration

--- src/mge/reflection/test/mock_call_context.hpp
+++ src/mge/reflection/test/mock_call_context.hpp
@@ -11,6 +11,17 @@ namespace mge::reflection {
     public:
         MOCK_call_context() = default;
         virtual ~MOCK_call_context() = default;
+        MOCK_METHOD(bool, bool_parameter, (size_t index), (override));
+        MOCK_METHOD(int8_t, int8_t_parameter, (size_t index), (override));
+        MOCK_METHOD(uint8_t, uint8_t_parameter, (size_t index), (override));
+        MOCK_METHOD(int16_t, int16_t_parameter, (size_t index), (override));
+        MOCK_METHOD(uint16_t, uint16_t_parameter, (size_t index), (override));
+        MOCK_METHOD(int32_t, int32_t_parameter, (size_t index), (override));
+        MOCK_METHOD(uint32_t, uint32_t_parameter, (size_t index), (override));
+        MOCK_METHOD(int64_t, int64_t_parameter, (size_t index), (override));
+        MOCK_METHOD(uint64_t, uint64_t_parameter, (size_t index), (override));
+        MOCK_METHOD(float, float_parameter, (size_t index), (override));
+        MOCK_METHOD(double, double_parameter, (size_t index), (override));
         MOCK_METHOD(void*, this_ptr, (), (override));
         MOCK_METHOD(void, bool_result, (bool value), (override));
         MOCK_METHOD(void, int8_t_result, (int8_t value), (override));
