62198dbb5152be62df46749f8cea618fd24335eb
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-11-23 10:17:56 +0100

    Add core_reflector module for core functions
    - Create core_reflector.cpp implementing reflector interface
    - Register debugging functions: breakpoint, is_debugger_present, breakpoint_if_debugging
    - Add mge_module_reflection_core and mge_module_reflection_std modules to CMakeLists.txt
    - Both modules built as MODULE libraries linked to mgereflection and mgecore

---
+++ src/mge/reflection/core_reflector.cpp
@@ -0,0 +1,30 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/reflection/function.hpp"
+#include "mge/reflection/module.hpp"
+#include "mge/reflection/reflector.hpp"
+
+#include "mge/core/debugging.hpp"
+
+namespace mge::reflection {
+
+    class core_reflector : public reflector
+    {
+    public:
+        core_reflector() = default;
+        ~core_reflector() = default;
+        void reflect() const override
+        {
+            auto mge = module("mge");
+            mge(function("breakpoint", &mge::breakpoint));
+            mge(function("is_debugger_present", &mge::is_debugger_present));
+            mge(function("breakpoint_if_debugging",
+                         &mge::breakpoint_if_debugging));
+        }
+    };
+
+    MGE_REGISTER_IMPLEMENTATION(core_reflector,
+                                mge::reflection::reflector,
+                                core);
+} // namespace mge::reflection
