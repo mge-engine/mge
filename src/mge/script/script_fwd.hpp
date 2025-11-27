f474b23fd9ecd9b083f3d5248411a4f76b5d21ab
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-11-27 22:25:55 +0100

    Scaffolding: scripting

---
+++ src/mge/script/script_fwd.hpp
@@ -0,0 +1,10 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "mge/core/memory.hpp"
+
+namespace mge {
+    MGE_DECLARE_REF(script_engine);
+    MGE_DECLARE_REF(script_context);
+} // namespace mge
