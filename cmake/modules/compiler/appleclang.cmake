1740c1db192c5a5c1671e9339154f8aada554168
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-11-30 09:08:44 +0100

    Add Apple Clang support in CMake configuration

--- cmake/modules/compiler/appleclang.cmake
+++ cmake/modules/compiler/appleclang.cmake
@@ -2,9 +2,8 @@
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
 
-IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    MESSAGE(STATUS "-- Configuring for GCC")
+IF(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    MESSAGE(STATUS "-- Configuring for Apple Clang")
     ADD_DEFINITIONS("-Wall")
     ADD_DEFINITIONS("-std=c++23")
-    ADD_DEFINITIONS("-Wno-unused-but-set-variable")
 ENDIF()
