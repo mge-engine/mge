e5c54ebe0a5e7a1d82c199afece22e6099266f96
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-12-13 13:24:50 +0100

    Add copyright notice to assimp.cmake file

--- cmake/modules/thirdparty/assimp.cmake
+++ cmake/modules/thirdparty/assimp.cmake
@@ -1,2 +1,5 @@
+# mge - Modern Game Engine
+# Copyright (c) 2017-2023 by Alexander Schroeder
+# All rights reserved.
 FIND_PACKAGE(assimp CONFIG REQUIRED)
 #   target_link_libraries(main PRIVATE assimp::assimp)
