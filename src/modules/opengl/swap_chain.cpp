0abab0d22870e3f854179355c77aef0091f1e48f
Alexander Schroeder <schroeder.alexander@googlemail.com>
2025-12-14 14:37:53 +0100

    Implement screenshot functionality in swap_chain and add screenshot method in triangle sample

--- src/modules/opengl/swap_chain.cpp
+++ src/modules/opengl/swap_chain.cpp
@@ -3,6 +3,7 @@
 // All rights reserved.
 #include "swap_chain.hpp"
 #include "mge/core/stdexceptions.hpp"
+#include "mge/graphics/memory_image.hpp"
 #include "render_context.hpp"
 
 namespace mge::opengl {
@@ -22,6 +23,41 @@ namespace mge::opengl {
 
     image_ref swap_chain::screenshot()
     {
-        MGE_THROW(not_implemented) << "Screenshot not yet implemented";
+        auto& rc = static_cast<render_context&>(context());
+        auto  extent = rc.extent();
+
+        // OpenGL uses RGBA format with 8 bits per channel
+        image_format format(image_format::data_format::RGBA, data_type::UINT8);
+        auto         img =
+            std::make_shared<memory_image>(format,
+                                           extent,
+                                           extent.width * extent.height * 4);
+
+        // Read pixels from the front buffer
+        glReadBuffer(GL_FRONT);
+        glReadPixels(0,
+                     0,
+                     static_cast<GLsizei>(extent.width),
+                     static_cast<GLsizei>(extent.height),
+                     GL_RGBA,
+                     GL_UNSIGNED_BYTE,
+                     img->data());
+
+        // Flip the image vertically (OpenGL origin is bottom-left, images
+        // expect top-left)
+        auto*  pixels = static_cast<uint8_t*>(img->data());
+        size_t row_size = extent.width * 4;
+        auto   temp_row = std::make_unique<uint8_t[]>(row_size);
+
+        for (uint32_t y = 0; y < extent.height / 2; ++y) {
+            uint8_t* top_row = pixels + y * row_size;
+            uint8_t* bottom_row = pixels + (extent.height - 1 - y) * row_size;
+
+            std::memcpy(temp_row.get(), top_row, row_size);
+            std::memcpy(top_row, bottom_row, row_size);
+            std::memcpy(bottom_row, temp_row.get(), row_size);
+        }
+
+        return img;
     }
 } // namespace mge::opengl
