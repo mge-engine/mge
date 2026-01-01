aa26a25f3bcc53f45d38a28498cacd8c34e587ed
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-01-01 10:30:38 +0100

    add frame buffer class

---
+++ src/mge/graphics/frame_buffer.cpp
@@ -0,0 +1,12 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/graphics/frame_buffer.hpp"
+
+namespace mge {
+
+    frame_buffer::frame_buffer(render_context& context)
+        : mge::context_object(context)
+    {}
+
+} // namespace mge
