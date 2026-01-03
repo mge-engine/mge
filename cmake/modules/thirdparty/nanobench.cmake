91a3f0b01714df0f1aecbc7f0166b3cd76349d54
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-01-03 16:48:59 +0100

    Integrate nanobench (#290)
    Remove homegrown and unstable benchmark framework in favor of nanobench.

---
+++ cmake/modules/thirdparty/nanobench.cmake
@@ -0,0 +1,7 @@
+# mge - Modern Game Engine
+# Copyright (c) 2017-2023 by Alexander Schroeder
+# All rights reserved.
+FIND_PACKAGE(nanobench CONFIG REQUIRED)
+#target_link_libraries(main PRIVATE nanobench::nanobench)
+
+
