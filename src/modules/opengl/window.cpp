17fce3b234fb92fc85beb1de5eed1552a7e8a765
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-01-11 10:22:43 +0100

    format

--- src/modules/opengl/window.cpp
+++ src/modules/opengl/window.cpp
@@ -10,9 +10,9 @@ namespace mge {
     MGE_USE_TRACE(OPENGL);
 
     namespace opengl {
-        window::window(mge::opengl::render_system&             system,
-                       const mge::extent&         extent,
-                       const mge::window_options& options)
+        window::window(mge::opengl::render_system& system,
+                       const mge::extent&          extent,
+                       const mge::window_options&  options)
             : platform::window(system, extent, options)
         {
 
@@ -25,8 +25,7 @@ namespace mge {
         {
             MGE_DEBUG_TRACE(OPENGL, "Creating render context");
             auto context =
-                std::make_shared<mge::opengl::render_context>(system,
-                                                              this);
+                std::make_shared<mge::opengl::render_context>(system, this);
             m_render_context = context;
         }
     } // namespace opengl
