77a398340ced663de1d641c0e898f82dab12700c
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-02-17 21:51:43 +0100

    Fix #154: Print D3D12_MESSAGE_ID as text instead of integer
    Add message_id_name() function that converts D3D12_MESSAGE_ID enum values
    to human-readable strings via a generated switch statement (988 unique values
    from d3d12sdklayers.h). Use it in the debug message callback instead of
    printing the raw integer value.

---
+++ src/modules/directx12/message_id.hpp
@@ -0,0 +1,9 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#pragma once
+#include "dx12.hpp"
+
+namespace mge::dx12 {
+    const char* message_id_name(D3D12_MESSAGE_ID id);
+} // namespace mge::dx12
