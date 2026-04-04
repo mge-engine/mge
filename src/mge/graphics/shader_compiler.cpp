e5f06cb27bb00243ca271d6ec4232b5a9c8262e1
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-04-04 18:49:40 +0200

    Add shader compiler scaffolding

---
+++ src/mge/graphics/shader_compiler.cpp
@@ -0,0 +1,12 @@
+// mge - Modern Game Engine
+// Copyright (c) 2017-2023 by Alexander Schroeder
+// All rights reserved.
+#include "mge/graphics/shader_compiler.hpp"
+
+namespace mge {
+
+    MGE_REGISTER_COMPONENT(shader_compiler);
+
+    shader_compiler::~shader_compiler() {}
+
+} // namespace mge
