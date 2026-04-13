e92224b825ac3b8e60e1d649a1989ec27f2ef96b
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-04-13 10:41:59 +0200

    Add table of contents

--- src/mge/core/dump.cpp
+++ src/mge/core/dump.cpp
@@ -26,60 +26,57 @@ namespace mge {
 
     static std::string dump_timestamp()
     {
-        auto now = std::chrono::system_clock::now();
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                      now.time_since_epoch()) %
-                  1000;
-        auto time = std::chrono::system_clock::to_time_t(now);
+        auto      now = std::chrono::system_clock::now();
+        auto      ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           now.time_since_epoch()) %
+                       1000;
+        auto      time = std::chrono::system_clock::to_time_t(now);
         struct tm tm_buf;
 #ifdef MGE_OS_WINDOWS
         localtime_s(&tm_buf, &time);
 #else
         localtime_r(&time, &tm_buf);
 #endif
-        return fmt::format(
-            fmt::runtime("{:%Y%m%d%H%M%S}{:03d}"),
-            tm_buf,
-            ms.count());
+        return fmt::format(fmt::runtime("{:%Y%m%d%H%M%S}{:03d}"),
+                           tm_buf,
+                           ms.count());
     }
 
     static std::string dump_timestamp_display()
     {
-        auto now = std::chrono::system_clock::now();
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                      now.time_since_epoch()) %
-                  1000;
-        auto time = std::chrono::system_clock::to_time_t(now);
+        auto      now = std::chrono::system_clock::now();
+        auto      ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           now.time_since_epoch()) %
+                       1000;
+        auto      time = std::chrono::system_clock::to_time_t(now);
         struct tm tm_buf;
 #ifdef MGE_OS_WINDOWS
         localtime_s(&tm_buf, &time);
 #else
         localtime_r(&time, &tm_buf);
 #endif
-        return fmt::format(
-            fmt::runtime("{:%Y-%m-%d %H:%M:%S}.{:03d} {:%z}"),
-            tm_buf,
-            ms.count(),
-            tm_buf);
+        return fmt::format(fmt::runtime("{:%Y-%m-%d %H:%M:%S}.{:03d} {:%z}"),
+                           tm_buf,
+                           ms.count(),
+                           tm_buf);
     }
 
     static std::string dump_utc_timestamp()
     {
-        auto now = std::chrono::system_clock::now();
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                      now.time_since_epoch()) %
-                  1000;
-        auto time = std::chrono::system_clock::to_time_t(now);
+        auto      now = std::chrono::system_clock::now();
+        auto      ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           now.time_since_epoch()) %
+                       1000;
+        auto      time = std::chrono::system_clock::to_time_t(now);
         struct tm tm_buf;
 #ifdef MGE_OS_WINDOWS
         gmtime_s(&tm_buf, &time);
 #else
         gmtime_r(&time, &tm_buf);
 #endif
-        return fmt::format(
-            fmt::runtime("{:%Y-%m-%d %H:%M:%S}.{:03d} UTC"),
-            tm_buf,
-            ms.count());
+        return fmt::format(fmt::runtime("{:%Y-%m-%d %H:%M:%S}.{:03d} UTC"),
+                           tm_buf,
+                           ms.count());
     }
 
     static std::string dump_filename()
@@ -92,8 +89,7 @@ namespace mge {
         return result;
     }
 
-    dump::dump(std::string_view          purpose,
-               std::pmr::memory_resource* resource)
+    dump::dump(std::string_view purpose, std::pmr::memory_resource* resource)
         : m_purpose(purpose, resource)
         , m_resource(resource)
     {
@@ -102,9 +98,25 @@ namespace mge {
 
     dump::~dump() {}
 
-    static void write_dump(const stacktrace&          st,
-                           std::pmr::memory_resource* resource =
-                               std::pmr::get_default_resource())
+    static std::pmr::string heading_anchor(std::string_view           text,
+                                           std::pmr::memory_resource* resource)
+    {
+        std::pmr::string anchor(resource);
+        for (char c : text) {
+            if (c == ' ') {
+                anchor += '-';
+            } else if (std::isalnum(static_cast<unsigned char>(c)) ||
+                       c == '-' || c == '_') {
+                anchor += static_cast<char>(
+                    std::tolower(static_cast<unsigned char>(c)));
+            }
+        }
+        return anchor;
+    }
+
+    static void write_dump(
+        const stacktrace&          st,
+        std::pmr::memory_resource* resource = std::pmr::get_default_resource())
     {
         auto filename = dump_filename();
 
@@ -120,29 +132,52 @@ namespace mge {
 
         doc.heading(1, "Crash Dump");
 
+        // Collect provider section names for TOC
+        std::pmr::vector<std::pmr::string> section_names(resource);
+        section_names.emplace_back(std::pmr::string("Stack Trace", resource));
+        dump_info_provider::implementations(
+            [&section_names, resource](std::string_view implementation_name) {
+                try {
+                    auto provider =
+                        dump_info_provider::create(implementation_name);
+                    if (provider) {
+                        section_names.emplace_back(
+                            provider->section_name(resource));
+                    }
+                } catch (...) {
+                    section_names.emplace_back(
+                        std::pmr::string(implementation_name, resource));
+                }
+            });
+
+        // Table of Contents
+        std::pmr::vector<std::pmr::string> toc_items(resource);
+        for (const auto& name : section_names) {
+            auto anchor = heading_anchor(name, resource);
+            toc_items.emplace_back(
+                doc.link(name,
+                         std::pmr::string("#", resource) + anchor.c_str()));
+        }
+        doc.heading(2, "Table of Contents").unordered_list(toc_items);
+
         std::pmr::vector<std::pmr::string> info_items(resource);
-        info_items.emplace_back(
-            std::pmr::string("Executable: ", resource) +
-            executable_name().c_str());
-        info_items.emplace_back(
-            std::pmr::string("Timestamp: ", resource) +
-            dump_timestamp_display().c_str());
+        info_items.emplace_back(std::pmr::string("Executable: ", resource) +
+                                executable_name().c_str());
+        info_items.emplace_back(std::pmr::string("Timestamp: ", resource) +
+                                dump_timestamp_display().c_str());
         info_items.emplace_back(
             std::pmr::string("Timestamp (UTC): ", resource) +
             dump_utc_timestamp().c_str());
 #ifdef MGE_OS_WINDOWS
-        info_items.emplace_back(
-            std::pmr::string("Process ID: ", resource) +
-            std::to_string(GetCurrentProcessId()).c_str());
+        info_items.emplace_back(std::pmr::string("Process ID: ", resource) +
+                                std::to_string(GetCurrentProcessId()).c_str());
 #else
-        info_items.emplace_back(
-            std::pmr::string("Process ID: ", resource) +
-            std::to_string(getpid()).c_str());
+        info_items.emplace_back(std::pmr::string("Process ID: ", resource) +
+                                std::to_string(getpid()).c_str());
 #endif
         doc.unordered_list(info_items);
 
-        doc.heading(2, "Stack Trace")
-            .code_block(fmt::format("{}", st));
+        doc.heading(2, "Stack Trace").code_block(fmt::format("{}", st));
 
         dump_info_provider::implementations(
             [&doc, resource](std::string_view implementation_name) {
