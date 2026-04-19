d7097142d8c6e29477e36d4b94e40542d9792480
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-04-19 18:04:01 +0200

    Improve test coverage

--- src/mge/core/test/test_parameter.cpp
+++ src/mge/core/test/test_parameter.cpp
@@ -9,6 +9,8 @@ MGE_DEFINE_PARAMETER(std::vector<int>,
                      test,
                      test_parameter_vec,
                      "a test parameter");
+MGE_DEFINE_PARAMETER_WITH_DEFAULT(
+    int, test, test_parameter_default, "param with default", 42);
 
 TEST(parameter, simple_access)
 {
@@ -63,4 +65,61 @@ TEST(parameter, vec)
     // EXPECT_EQ(std::string("1, 2, 3"),
     //           MGE_PARAMETER(test, test_parameter_vec).to_string());
     // MGE_PARAMETER(test, test_parameter_vec).reset();
+}
+
+TEST(parameter, change_handler)
+{
+    bool changed = false;
+    MGE_PARAMETER(test, test_parameter1).set_change_handler([&changed]() {
+        changed = true;
+    });
+    MGE_PARAMETER(test, test_parameter1).notify_change();
+    EXPECT_TRUE(changed);
+    MGE_PARAMETER(test, test_parameter1).set_change_handler(nullptr);
+}
+
+TEST(parameter, default_value)
+{
+    EXPECT_FALSE(MGE_PARAMETER(test, test_parameter_default).has_value());
+    EXPECT_EQ(42, MGE_PARAMETER(test, test_parameter_default).get());
+}
+
+TEST(parameter, default_value_override)
+{
+    MGE_PARAMETER(test, test_parameter_default).value() = 99;
+    EXPECT_TRUE(MGE_PARAMETER(test, test_parameter_default).has_value());
+    EXPECT_EQ(99, MGE_PARAMETER(test, test_parameter_default).get());
+    MGE_PARAMETER(test, test_parameter_default).reset();
+    EXPECT_FALSE(MGE_PARAMETER(test, test_parameter_default).has_value());
+    EXPECT_EQ(42, MGE_PARAMETER(test, test_parameter_default).get());
+}
+
+TEST(parameter, get_with_fallback)
+{
+    EXPECT_FALSE(MGE_PARAMETER(test, test_parameter_default).has_value());
+    EXPECT_EQ(100, MGE_PARAMETER(test, test_parameter_default).get(100));
+    MGE_PARAMETER(test, test_parameter_default).reset();
+}
+
+TEST(parameter, json_read_write)
+{
+    mge::json::json j;
+    j["test"]["test_parameter1"] = true;
+    MGE_PARAMETER(test, test_parameter1)
+        .read_value(j["test"]["test_parameter1"]);
+    EXPECT_TRUE(MGE_PARAMETER(test, test_parameter1).has_value());
+    EXPECT_TRUE(MGE_PARAMETER(test, test_parameter1).get());
+
+    mge::json::json out;
+    MGE_PARAMETER(test, test_parameter1).write_value(out);
+    EXPECT_TRUE(out.get<bool>());
+
+    MGE_PARAMETER(test, test_parameter1).reset();
+}
+
+TEST(parameter, as_typed)
+{
+    MGE_PARAMETER(test, test_parameter1).set_value(true);
+    EXPECT_TRUE(MGE_PARAMETER(test, test_parameter1).as<bool>());
+    MGE_PARAMETER(test, test_parameter1).reset();
 }
