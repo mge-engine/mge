d93397aa825090c6c4bee389fc800c09446c9def
Alexander Schroeder <schroeder.alexander@googlemail.com>
2026-04-22 07:40:53 +0200

    Update vcpkg baseline (#340)
    Update vcpkg baseline from `c2a21b787cc505d5960d0292d30fd14306b07dd4` to
    `3fb54e6717ea6a48099b02798e82fafefb2d410e`.
    
    Co-authored-by: github-actions <github-actions@github.com>

--- tools/ci/vcpkg_version.py
+++ tools/ci/vcpkg_version.py
@@ -1,5 +1,5 @@
 # mge - Modern Game Engine
 # Copyright (c) 2017-2023 by Alexander Schroeder
 # All rights reserved.
-version = "51"
-vcpkg_githash = "c2a21b787cc505d5960d0292d30fd14306b07dd4"
+version = "52"
+vcpkg_githash = "3fb54e6717ea6a48099b02798e82fafefb2d410e"
